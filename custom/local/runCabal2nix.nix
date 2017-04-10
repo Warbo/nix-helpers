b622ca6a8a065b64446bc9675ca3c9c3d435a8f7
Chris Warburton <chriswarbo@gmail.com>
2017-04-10 14:43:48 +0100

    Work around cabal2nix breaking for git repos

--- custom/local/runCabal2nix.nix
+++ custom/local/runCabal2nix.nix
@@ -1,12 +1,12 @@
-{ cabal2nix, glibcLocales, hackageDb, nix, runCommand }:
+{ cabal2nix, glibcLocales, hackageDb, latestGit, lib, nix, runCommand }:
 
 with builtins;
+with lib;
 
 { url, args ? [] }:
 
 runCommand "run-cabal2nix"
   {
-    inherit url;
     inherit hackageDb;
     NIX_REMOTE  = "daemon";
     NIX_PATH    = builtins.getEnv "NIX_PATH";
@@ -15,8 +15,15 @@ runCommand "run-cabal2nix"
     # Otherwise cabal2nix dies for accented characters
     LANG           = "en_US.UTF-8";
     LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+
+    # cabal2nix version 20160308 breaks for git repos by trying to import
+    # nixexprs as JSON; we work around this by fetching separately.
+    url = if hasPrefix "http" url && hasSuffix ".git" url
+             then latestGit { inherit url; }
+             else url;
   }
   ''
+    set -e
     export HOME="$PWD/home"
     cp -r "$hackageDb" ./home
     chmod 777 ./home
