9daa2d882f345f4f89933fb06087bbb02f1eeb76
Chris Warburton <chriswarbo@gmail.com>
2017-06-09 14:04:50 +0100

    Use stableHackageDb by default in runCabal2nix

--- custom/local/runCabal2nix.nix
+++ custom/local/runCabal2nix.nix
@@ -1,13 +1,26 @@
-{ cabal2nix, glibcLocales, hackageDb, latestGit, lib, runCommand, withNix }:
+{ cabal2nix, glibcLocales, latestGit, lib, runCommand, stableHackageDb,
+  withNix }:
 
 with builtins;
 with lib;
 
-{ url, args ? [] }:
+{
+  # Package location, containing a .cabal file, e.g. path, git repo, "cabal://"
+  url,
 
-trace "FIXME: Can we use callCabal2nix yet?" runCommand "run-cabal2nix"
+  # Extra arguments for cabal2nix command. NOTE: you must quote things yourself,
+  # if needed. Also, the available options may vary between cabal2nix versions.
+  args ? [],
+
+  # Hackage repo contents to use. Get the latest content (via 'cabal update') by
+  # using hackageDb, but this doesn't cache well. Defaults to stableHackageDb,
+  # which is built from a fixed git revision and hence caches well.
+  packageDb ? stableHackageDb
+}:
+
+runCommand "run-cabal2nix"
   (withNix {
-    inherit hackageDb;
+    inherit packageDb;
     buildInputs = [ cabal2nix ];
 
     # Otherwise cabal2nix dies for accented characters
@@ -23,7 +36,7 @@ trace "FIXME: Can we use callCabal2nix yet?" runCommand "run-cabal2nix"
   ''
     set -e
     export HOME="$PWD/home"
-    cp -r "$hackageDb" ./home
+    cp -r "$packageDb" ./home
     chmod 777 ./home
 
     cabal2nix ${concatStringsSep " " args} "$url" > "$out"
