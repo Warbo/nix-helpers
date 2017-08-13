5df6b838a8e447da23fdb351f1f351de8c1ac6fa
Chris Warburton <chriswarbo@gmail.com>
2017-08-13 15:50:43 +0100

    Don't force wrap dependencies at eval time
    Previously, wrap was splicing the given paths and variables into the build
    command, which required building any given derivations at eval time in order to
    construct the buildCommand string. This is less than ideal!
    
    We now add a layer of indirection: paths and variables are added to the
    environment of buildCommand, rather than spliced in; these values are added
    "as-is", so that derivations are left untouched.
    
    This requires us to perform all of our manipulation (escaping, etc.) in Bash, at
    build time, rather than in Nix like we were doing before. This is a little ugly,
    and requires a few levels of indirection, but the tests are all
    passing (including new ones which verify that the paths and vars aren't forced,
    by using some broken derivations which would cause Nix to bail if they ever did
    get forced)

--- custom/local/wrap.nix
+++ custom/local/wrap.nix
@@ -3,8 +3,39 @@
 with builtins;
 with lib;
 with rec {
+  checks = varChk // depChk;
+
+  # Make sure that derivations given as paths and vars aren't forced during
+  # evaluation (only at build time)
+  depChk =
+    with {
+      script = wrap {
+        vars = {
+          broken1 = runCommand "broken1" {} "exit 1";
+        };
+
+        paths = [ (runCommand "broken2" {} "exit 1") ];
+
+        script = "exit 1";
+      };
+    };
+    {
+      brokenDepsNotForced = runCommand "checkBrokenDepsNotForced"
+        {
+          val = if isString script.buildCommand then "true" else "false";
+        }
+        ''
+          if "$val"
+          then
+            echo "pass" > "$out"
+            exit 0
+          fi
+          exit 1
+        '';
+    };
+
   # Try a bunch of strings with quotes, spaces, etc. and see if they survive
-  checks = mapAttrs (n: v: runCommand "wrap-escapes-${n}"
+  varChk = mapAttrs (n: v: runCommand "wrap-escapes-${n}"
                              {
                                cmd = wrap rec {
                                  vars   = { "${n}" = v; };
@@ -34,56 +65,96 @@ with rec {
                       MEDLEY = ''with" all 'of the" above'';
                     };
 
-  # makeWrapper has some horrible quoting issues. In particular:
-  #  - Values are spliced into the resulting script as-is, i.e. without any
-  #    escaping. Hence we need to escape them ourselves.
-  #  - Escaping in bash is done by wrapping in single quotes and replacing any
-  #    single quotes in the string with '\'' (the first ' closes the first part
-  #    of the string, the following \' is the literal quote that we want (in
-  #    bash, juxtaposition is string concatenation), then we begin the rest of
-  #    the string using the final '.
-  #  - Since makeWrapper is a bash function, we have to call it from bash, and
-  #    that requires escaping our values again (the call to makeWrapper will
-  #    strip off one layer of escaping; the second layer will be stripped off
-  #    when the parsing script is being parsed).
-  #  - Values (but not names) automatically get wrapped in "double quotes"; this
-  #    messes up our escaping: one the one hand, it causes our single quotes to
-  #    all appear as literals, which we don't want; on the other hand, it causes
-  #    many potential inputs (e.g. things containing $) to be mangled, which is
-  #    precisely what we wanted to avoid!
-  #  - To avoid these double quotes, we surround our double-escaped string with
-  #    double quotes; hence 'foo' becomes "'foo'", which when wrapped in double
-  #    quotes becomes ""'foo'"", which bash will parse as three strings: an
-  #    empty string "", a string 'foo' and an empty string "". Concatenating
-  #    these is equivalent to the 'foo' that we wanted.
-  #  - Pro tip to any readers: try to avoid unintended string interpretation
-  #    wherever you can. Instead of "quoting variables where necessary", you
-  #    should always quote all variables; instead of embedding raw strings into
-  #    generated scripts and sprinkling around some quote marks, you should
-  #    always escape them properly (replace single quotes with '\'' and wrap in
-  #    single quotes); never treat double quotes as an escaping mechanism.
-  set = n: v:
-    with rec {
-      en =          escapeShellArg (escapeShellArg "${n}");
-      ev = "'\"'" + escapeShellArg (escapeShellArg "${v}") + "'\"'";
-    };
-    "--set ${en} ${ev}";
-
   wrap = { paths ? [], vars ? {}, file ? null, script ? null, name ? "wrap" }:
     assert file != null || script != null ||
            abort "wrap needs 'file' or 'script' argument";
     with rec {
-      f    = if file == null then writeScript name script else file;
-      args = (map (p: "--prefix PATH : ${p}/bin") paths) ++
-             (attrValues (mapAttrs set vars));
+      f = if file == null then writeScript name script else file;
+
+      # Store each path in a variable pathVarN
+      pathArgs = foldl' (rest: path: {
+                          count = rest.count + 1;
+                          args  = rest.args // {
+                            "pathVar${toString rest.count}" = path;
+                          };
+                        })
+                        { count = 1; args = {}; }
+                        paths;
+
+      # Store each name in a variable varNameN and the corresponding value in a
+      # variable varVarN
+      varArgs = foldl' (rest: name: {
+                         count = rest.count + 1;
+                         names = rest.names // {
+                           "varName${toString rest.count}" = name;
+                         };
+                         args  = rest.args // {
+                           "varVar${toString rest.count}" = getAttr name vars;
+                         };
+                       })
+                       { count = 1; names = {}; args = {}; }
+                       (attrNames vars);
     };
     runCommand name
-      {
+      (pathArgs.args // varArgs.args // varArgs.names // {
         inherit f;
+        pathCount   = length paths;
+        varCount    = length (attrNames vars);
         buildInputs = [ makeWrapper ];
-      }
+      })
       ''
-        makeWrapper "$f" "$out" ${concatStringsSep " " args}
+        ARGS=()
+
+        echo "Getting paths" 1>&2
+        for N in $(seq 1 "$pathCount")
+        do
+          # Use a "variable variable" to look up "$pathVar$N" as a variable name
+           ARG="pathVar$N"
+          ARGS=("''${ARGS[@]}" "--prefix" "PATH" ":" "''${!ARG}/bin")
+        done
+
+        echo "Getting vars" 1>&2
+        for N in $(seq 1 "$pathCount")
+        do
+          # Use "variable variables" like with paths, but for the name and value
+          NAME="varName$N"
+           ARG="varVar$N"
+
+          # makeWrapper doesn't escape properly, so spaces, quote marks, dollar
+          # signs, etc. will cause errors. Given a value FOO, makeWrapper will
+          # write out a script containing "FOO" (i.e. it wraps the text in
+          # double quotes). Double quotes aren't safe in Bash, since they splice
+          # in variables for dollar signs, etc. Plus, makeWrapper isn't actually
+          # doing any escaping: if our text contains a ", then it will appear
+          # verbatim and break the surrounding quotes.
+          # To work around this we do the following:
+          #  - Escape all single quotes in our value using sed; this is made
+          #    more awkward since we're using single-quoted Nix strings...
+          #  - Surround this escaped value in single quotes, hence making a
+          #    fully escaped text value which won't mess up any content
+          #  - Surround this single-quoted-and-escaped value in double quotes.
+          #    These "cancel out" the double quotes added by makeWrapper, i.e.
+          #    instead of FOO -> "FOO", we do "FOO" -> ""FOO"", and hence the
+          #    value FOO (in this case, our single-quoted-escaped-value) appears
+          #    OUTSIDE the double quotes, and is hence free to use single quotes
+
+          # Pro tip to any readers: try to avoid unintended string
+          # interpretation wherever you can. Instead of "quoting variables where
+          # necessary", you should always quote all variables; instead of
+          # embedding raw strings into generated scripts and sprinkling around
+          # some quote marks, you should always escape them properly (in Bash,
+          # this is done by escaping single quotes wrapping in single quotes);
+          # never treat double quotes as an escaping mechanism.
+
+           VAL="''${!ARG}"
+            BS='\'
+             T="'"
+           ESC=$(echo "$VAL" | sed -e "s/$T/$T$BS$BS$T$T/g")
+
+          ARGS=("''${ARGS[@]}" "--set" "''${!NAME}" "\"'$ESC'\"")
+        done
+
+        makeWrapper "$f" "$out" "''${ARGS[@]}"
       '';
 };
 
