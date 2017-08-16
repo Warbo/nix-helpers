e3966a3f89d588543269554d3bf45c952697a284
Chris Warburton <chriswarbo@gmail.com>
2017-08-17 00:29:27 +0100

    Split up "helpers" into individual packages

---
+++ custom/local/dirContaining.nix
@@ -0,0 +1,22 @@
+# Create a directory containing 'files'; the directory structure will be
+# relative to 'base', for example:
+#
+#   dirContaining /foo/bar [ /foo/bar/baz /foo/bar/quux/foobar ]
+#
+# Will produce a directory containing 'baz' and 'quux/foobar'.
+{ mergeDirs, runCommand }:
+
+with builtins;
+base: files:
+  mergeDirs (map (f: runCommand "dir"
+                       {
+                         base = toString base;
+                         file = toString base + "/${f}";
+                       }
+                       ''
+                         REL=$(echo "$file" | sed -e "s@$base/@@g")
+                         DIR=$(dirname "$REL")
+                         mkdir -p "$out/$DIR"
+                         ln -s "$file" "$out/$REL"
+                       '')
+                 files)
