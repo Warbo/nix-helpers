87abf7ad6c1e604f8ff5b4b1d93d5e5a2cb7af08
Chris Warburton <chriswarbo@gmail.com>
2017-08-17 00:30:04 +0100

    Make more packages "local" (for local people)

--- custom/local/withLatestGit.nix
+++ custom/local/withLatestGit.nix
@@ -1,10 +1,10 @@
-self: super:
+{ latestGit, lib, stdenv, substituteAll }:
 
-{
+with builtins;
+with lib;
 
 # Use latestGit as src for a derivation, cache the commit ID in the environment
-withLatestGit = { url, ref ? "HEAD", refIsRev ? false, srcToPkg, resultComposes ? false }:
-with builtins;
+{ url, ref ? "HEAD", refIsRev ? false, srcToPkg, resultComposes ? false }:
 
 assert isFunction srcToPkg;
 assert isString url;
@@ -13,42 +13,41 @@ assert isBool refIsRev;
 assert isBool resultComposes;
 assert refIsRev -> ref != "HEAD";
 
-let rawSource = self.latestGit { inherit url;
-                                 ref = if refIsRev then "" else ref; };
-    source    = if refIsRev then self.stdenv.lib.overrideDerivation
-                                   rawSource
-                                   (old: { rev = ref; })
-                            else rawSource;
-    result    = srcToPkg source;
-    hUrl      = builtins.hashString "sha256" url;
-    hRef      = builtins.hashString "sha256" ref;
-    rev       = if refIsRev then ref else source.rev;
-in
+with rec {
+  rawSource = latestGit { inherit url;
+                          ref = if refIsRev then "" else ref; };
+  source    = if refIsRev then stdenv.lib.overrideDerivation
+                                 rawSource
+                                 (old: { rev = ref; })
+                          else rawSource;
+  result    = srcToPkg source;
+  hUrl      = builtins.hashString "sha256" url;
+  hRef      = builtins.hashString "sha256" ref;
+  rev       = if refIsRev then ref else source.rev;
+};
 
 assert isAttrs source;
 assert hasAttr "rev" source;
 assert isAttrs result || isFunction result;
 assert resultComposes -> isFunction result;
 
-let cacheRev = p:
-      assert isAttrs p;
-      self.lib.overrideDerivation p (old: {
-        setupHook = self.substituteAll {
-          src = ./nixGitRefs.sh;
-          key = "${hUrl}_${hRef}";
-          val = rev;
-        };
-      });
-    drv = if isFunction result
-             then if resultComposes
-                     then result cacheRev
-                     else args: cacheRev (result args)
-             else cacheRev result;
-in
+with rec {
+  cacheRev = p:
+    assert isAttrs p;
+    overrideDerivation p (old: {
+      setupHook = substituteAll {
+        src = ./nixGitRefs.sh;
+        key = "${hUrl}_${hRef}";
+        val = rev;
+      };
+    });
+  drv = if isFunction result
+           then if resultComposes
+                   then result cacheRev
+                   else args: cacheRev (result args)
+           else cacheRev result;
+};
 
 assert isFunction result -> isFunction drv;
 assert isAttrs    result -> isAttrs    drv;
-
-drv;
-
-}
+drv
