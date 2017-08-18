33e3c47fc28470424194c38ff66d40257b376651
Chris Warburton <chriswarbo@gmail.com>
2017-08-18 17:20:56 +0100

    Refactor attrsToDirs to use nixListToBashArray
    This prevents derivations getting build at eval time (previously, we were
    splicing them into strings, which can cause such problems, and also interacts
    badly with nix-instantiate).
    
    I've also changed the implementation of fail, to prevent causing a circular
    dependency.

--- custom/local/attrsToDirs.nix
+++ custom/local/attrsToDirs.nix
@@ -1,7 +1,7 @@
 # Builds a directory whose entries/content correspond to the names/values of
 # the given attrset. When a value is an attrset, the corresponding entry is
 # a directory, whose contents is generated with attrsToDirs on that value.
-{ isPath, lib, runCommand }:
+{ isPath, lib, nixListToBashArray, nothing, runCommand }:
 
 with builtins;
 with lib;
@@ -10,18 +10,49 @@ with rec {
     if isPath val || isDerivation val
        then [{ name  = prefix;
                value = val; }]
-       else concatMap (entry: toPaths (prefix + "/" + entry)
-                                      (getAttr entry val))
-                      (attrNames val);
-
-  toCmds = attrs:
-    concatStringsSep "\n"
-      ([''mkdir -p "$out"''] ++
-       (map (entry: ''
-               mkdir -p "$(dirname "${entry.name}")"
-               ln -s "${entry.value}" "${entry.name}"
-             '')
-             (toPaths "$out" attrs)));
+       else if isAttrs val
+               then concatMap (entry: toPaths (prefix + "/" + entry)
+                                              (getAttr entry val))
+                              (attrNames val)
+               else abort "Unsupported type ${typeOf val} in attrsToDirs";
 };
 
-attrs: trace "FIXME: Use env vars instead of splicing" runCommand "merged" {} (toCmds attrs)
+attrs:
+  with rec {
+    # We can't have empty attr names, so always stick a dummy '_' at the start,
+    # and strip it off in the build script
+    data = listToAttrs (toPaths "_" attrs);
+
+    # The element order of these lists is arbitrary, but they must match
+    paths    = attrNames data;
+    content  = map (n: getAttr n data) paths;
+
+    # Create bash code and env vars for the build command, without forcing any
+    # derivations to be built at eval time.
+    pathData    = nixListToBashArray { name = "VALPATHS"; args = paths;   };
+    contentData = nixListToBashArray { name = "VALUES";   args = content; };
+  };
+  if attrs == {}
+     then nothing
+     else runCommand "merged" (pathData.env // contentData.env) ''
+            ${pathData.code}
+            ${contentData.code}
+
+            # Loop over each path/content entry in our arrays (off-by-one cruft
+            # is due to seq preferring to count from 1)
+            for NPLUSONE in $(seq 1 "''${#VALUES[@]}")
+            do
+              N=$(( NPLUSONE - 1 ))
+
+              # The output path we're going to make; cut off leading _ and
+              # prepend $out
+              STRIPPED=$(echo "''${VALPATHS[$N]}" | cut -c 2-)
+                     P="$out$STRIPPED"
+
+              # Ensure parent directories exist
+              mkdir -vp "$(dirname "$P")"
+
+              # Link value in place (saves space compared to copying)
+              ln -vs "''${VALUES[$N]}" "$P"
+            done
+          ''
