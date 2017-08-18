d040141da9ada37af560d9b15a5a9a3e12dc65f0
Chris Warburton <chriswarbo@gmail.com>
2017-08-18 17:20:56 +0100

    Refactor attrsToDirs to use nixListToBashArray
    This prevents derivations getting build at eval time (previously, we were
    splicing them into strings, which can cause such problems, and also interacts
    badly with nix-instantiate).
    
    I've also changed the implementation of fail, to prevent causing a circular
    dependency.

--- custom/local/fail.nix
+++ custom/local/fail.nix
@@ -1,11 +1,13 @@
-{ attrsToDirs, writeScript }:
+{ runCommand, writeScript }:
 
-attrsToDirs {
-  bin = {
-    fail = writeScript "error-logger" ''
-      #!/usr/bin/env bash
-      echo -e "$*" 1>&2
-      exit 1
-    '';
-  };
-}
+with {
+  script = writeScript "error-logger" ''
+    #!/usr/bin/env bash
+    echo -e "$*" 1>&2
+    exit 1
+  '';
+};
+runCommand "mk-fail" { inherit script; } ''
+  mkdir -p "$out/bin"
+  cp "$script" "$out/bin/fail"
+''
