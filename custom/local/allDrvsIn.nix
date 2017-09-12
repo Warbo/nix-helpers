f243e1397381d79c1db0a5c8877b4eb783ef1fc1
Chris Warburton <chriswarbo@gmail.com>
2017-09-12 15:45:28 +0100

    Add test for allDrvsIn

--- custom/local/allDrvsIn.nix
+++ custom/local/allDrvsIn.nix
@@ -1,3 +1,29 @@
-{ lib }:
+# Traverse nested attribute sets, gathering a list of all derivations we find
+{ lib, nothing }:
 
-lib.collect lib.isDerivation
+with builtins;
+with lib;
+with rec {
+  go = collect isDerivation;
+
+  testData = rec {
+    input = {
+      a = 5;
+      b = null;
+      c = true;
+      d = {};
+      e = "hello";
+      f = { x = nothing; y = nothing; g = []; };
+      z = nothing;
+    };
+
+    message      = "gotLength should equal shouldLength";
+    output       = go input;
+    gotLength    = length output;
+    shouldLength = 3;
+  };
+
+  # We should ignore everything except the derivations ('nothing')
+  test = testData.gotLength == testData.shouldLength || abort (toJSON testData);
+};
+assert test; go
