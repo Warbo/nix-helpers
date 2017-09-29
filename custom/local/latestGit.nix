eeb26d5dec2b89cae1a586ae630e554fe5ff7e2f
Chris Warburton <chriswarbo@gmail.com>
2017-09-29 05:52:52 +0100

    BREAKING CHANGE: Don't allow latestGit when 'stable' is true
    Fetching the latest version of some repo defeats the point of using a stable
    version of nix-config, hence we forbid the use of latestGit in these cases; with
    two exceptions:
    
     - latestNixCfg gives access to an unstable nix-config, from within a stable one
     - runCabal2nix has a workaround for dodgy git handling in cabal2nix, such that
       we check out a given repo first, then call cabal2nix on that directory. Since
       we have no commit info for these, we allow latestGit, with a warning (since
       such usages should really be replaced by an explicit call to latestGit, with
       a proper fallback, etc.)
    
    Notice that this causes a lot of derivations to change, and it will cause
    external projects which call into this repo break as well. Going forward, that
    should be less of a problem, since they can use an *actually* stable version.

--- custom/local/latestGit.nix
+++ custom/local/latestGit.nix
@@ -11,48 +11,65 @@
 #
 # TODO: This duplicates some functionality of fetchgitrevision; wait for that
 # API to settle down, then use it here.
+{ cacert, fetchgit, fetchGitHashless, git, lib, runCommand, sanitiseName,
+  stable }:
+with builtins // lib // { configIsStable = stable; };
 
-{ cacert, fetchGitHashless, git, gnused, runCommand, sanitiseName }:
+# We need the url, but ref is optional (e.g. if we want a particular branch).
+# If nix-config is in stable mode, 'stable' should have a 'rev' and a 'sha256'.
+{ url, ref ? "HEAD", stable ? {} }@args:
 
-with builtins;
+with rec {
+  # In stable mode, we use the rev and sha256 hard-coded in 'stable'
+  stableRepo = fetchgit {
+    inherit url;
+    inherit (stable) rev sha256;
+  };
 
-# We need the url, but ref is optional (e.g. if we want a particular branch)
-{ url, ref ? "HEAD" }@args:
-  with rec {
-    # We allow refs to be given in two ways: as a standalone env var...
-    key    = "${hashString "sha256" url}_${hashString "sha256" ref}";
-    keyRev = getEnv "nix_git_rev_${key}";
+  # In unstable mode, we look up the latest 'rev' dynamically
+  unstableRepo = fetchGitHashless (removeAttrs (args // { inherit rev; })
+                                               [ "ref" "stable" ]);
 
-    # Or as an entry in a JSON table
-    repoRefStr = getEnv "REPO_REFS";
-    repoRefs   = if repoRefStr == ""
-                    then {}
-                    else fromJSON repoRefStr;
+  # 'rev' can be given by the env vars 'REPO_REFS' or 'nix_git_rev_...'. If not
+  # found in either, we run 'newRev' to query the URL for the latest version.
+  rev = if hasAttr url repoRefs
+           then getAttr url repoRefs
+                else if keyRev == ""
+                        then newRev
+                        else keyRev;
 
-    # Get the commit ID for the given ref in the given repo.
-    newRev = import (runCommand
-      "repo-${sanitiseName ref}-${sanitiseName url}"
-      {
-        inherit ref url;
+  # The 'REPO_REFS' env var makes it easy to specify a bunch of revs at once
+  repoRefStr = getEnv "REPO_REFS";
+  repoRefs   = if repoRefStr == ""
+                  then {}
+                  else fromJSON repoRefStr;
 
-        # Avoids caching. This is a cheap operation and needs to be up-to-date
-        version = toString currentTime;
 
-        # Required for SSL
-        GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+  # The 'nix_git_rev_...' env vars make it easy to specify one individual 'rev'
+  key    = "${hashString "sha256" url}_${hashString "sha256" ref}";
+  keyRev = getEnv "nix_git_rev_${key}";
 
-        buildInputs = [ git gnused ];
-      }
-      ''
-        REV=$(git ls-remote "$url" "$ref") || exit 1
+  # Get the commit ID for the given ref in the given repo. Takes a few seconds.
+  newRev = import (runCommand "repo-${sanitiseName ref}-${sanitiseName url}"
+    {
+      inherit ref url;
+      cacheBuster    = toString currentTime;
+      GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+      buildInputs    = [ git ];
+    }
+    ''
+      set -o pipefail
+      # Commit ID is first (awk) 'field' in the first 'record'. Wrap in quotes.
+      git ls-remote "$url" "$ref" | awk 'NR==1 {print "\""$1"\""}' > "$out"
+    '');
 
-        printf '"%s"' $(echo "$REV"        |
-                        head -n1           |
-                        sed -e 's/\s.*//g' ) > "$out"
-      '');
+  # Logic for choosing between stable and unstable
+  useStable  = configIsStable && !unsafeSkip;
+  unsafeSkip = stable.unsafeSkip or false;
 
-    rev = repoRefs.url or (if keyRev == ""
-                              then newRev
-                              else keyRev);
+  error = msg: abort (toJSON { inherit msg url ref stable; });
 };
-fetchGitHashless (removeAttrs (args // { inherit rev; }) [ "ref" ])
+
+assert useStable -> stable ? rev    || error "No stable rev";
+assert useStable -> stable ? sha256 || error "No stable sha256";
+if useStable then stableRepo else unstableRepo
