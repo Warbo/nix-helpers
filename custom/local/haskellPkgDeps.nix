60a33c88ecaa76f142954f508d7b2a249279243c
Chris Warburton <chriswarbo@gmail.com>
2017-12-20 01:41:27 +0000

    Take in Haskell pkg set

--- custom/local/haskellPkgDeps.nix
+++ custom/local/haskellPkgDeps.nix
@@ -1,70 +1,70 @@
 { cabal-install, haskell, jq, lib, runCommand, stableHackageDb, utillinux }:
 
+with lib;
+
 {
   dir,
   extra-sources   ? [],
   hackageContents ? stableHackageDb,
   name            ? "pkg"
+  pkgs
 }:
-with lib;
-with rec {
-  go = pkgs: runCommand "haskell-${name}-deps.nix"
-    {
-      inherit dir hackageContents;
-      buildInputs  = [ cabal-install jq pkgs.ghc utillinux ];
-      extraSources = concatStringsSep "\n" extra-sources;
-      expr         = ''
-        with builtins;
-        listToAttrs (map (nv: { name = elemAt nv 0; value = elemAt nv 1; })
-                         (fromJSON (readFile ./versions.json)))
-      '';
-    }
-  ''
-    set -e
-    set -o pipefail
 
-    cp -r "$dir" ./src
-    chmod +w -R ./src
+import (runCommand "haskell-${name}-deps.nix"
+  {
+    inherit dir hackageContents;
+    buildInputs  = [ cabal-install jq pkgs.ghc utillinux ];
+    extraSources = concatStringsSep "\n" extra-sources;
+    expr         = ''
+      with builtins;
+      listToAttrs (map (nv: { name = elemAt nv 0; value = elemAt nv 1; })
+                       (fromJSON (readFile ./versions.json)))
+    '';
+  }
+''
+  set -e
+  set -o pipefail
 
-    export HOME="$PWD/cache"
-    mkdir -p "$HOME"/.cabal/packages/hackage.haskell.org
-    ln -s "$hackageContents"/.cabal/packages/hackage.haskell.org/00-index.tar \
-                     "$HOME"/.cabal/packages/hackage.haskell.org/
+  cp -r "$dir" ./src
+  chmod +w -R ./src
 
-    cd ./src
-    cabal sandbox init
-    while read -r SRC
+  export HOME="$PWD/cache"
+  mkdir -p "$HOME"/.cabal/packages/hackage.haskell.org
+  ln -s "$hackageContents"/.cabal/packages/hackage.haskell.org/00-index.tar \
+                   "$HOME"/.cabal/packages/hackage.haskell.org/
+
+  cd ./src
+  cabal sandbox init
+  while read -r SRC
+  do
+    cabal sandbox add-source "$SRC"
+  done < <(echo "$extraSources")
+
+  GOT=$(cabal install --dry-run           \
+                      --enable-tests      \
+                      --enable-benchmarks) ||
+  {
+    echo "$GOT" 1>&2
+    echo "Error listing cabal dependencies" 1>&2
+    exit 1
+  }
+
+  MSG='the following would be installed'
+  L=$(echo "$GOT" | grep -A 9999999 "$MSG" | tail -n+2 | tr -d ' ')
+
+  function format {
+    while read -r DEP
     do
-      cabal sandbox add-source "$SRC"
-    done < <(echo "$extraSources")
+      NAME=$(echo "$DEP" | rev | cut -d '-' -f2- | rev)
+      echo "\"$NAME\" = runCabal2nix {
+                          name = \"$NAME\";
+                          url  = \"cabal://$DEP\";
+                        };"
+    done
+  }
 
-    GOT=$(cabal install --dry-run           \
-                        --enable-tests      \
-                        --enable-benchmarks) ||
-    {
-      echo "$GOT" 1>&2
-      echo "Error listing cabal dependencies" 1>&2
-      exit 1
-    }
-
-    MSG='the following would be installed'
-    L=$(echo "$GOT" | grep -A 9999999 "$MSG" | tail -n+2 | tr -d ' ')
-
-    function format {
-      while read -r DEP
-      do
-        NAME=$(echo "$DEP" | rev | cut -d '-' -f2- | rev)
-        echo "\"$NAME\" = runCabal2nix {
-                            name = \"$NAME\";
-                            url  = \"cabal://$DEP\";
-                          };"
-      done
-    }
-
-    echo '['                                         >  "$out"
-      echo "$L" | head -n-1 | jq -R '"cabal://" + .' >> "$out"
-      echo "\"$dir\""                                >> "$out"
-    echo ']'                                         >> "$out"
-  '';
-};
-mapAttrs (_: go) haskell.packages
+  echo '['                                         >  "$out"
+    echo "$L" | head -n-1 | jq -R '"cabal://" + .' >> "$out"
+    echo "\"$dir\""                                >> "$out"
+  echo ']'                                         >> "$out"
+'')
