5d9e479a207bd16138890eeb5e205299764a533c
Chris Warburton <chriswarbo@gmail.com>
2018-01-10 15:24:52 +0000

    Handle extra-sources properly in haskellPkgDeps

--- custom/local/haskellPkgDeps.nix
+++ custom/local/haskellPkgDeps.nix
@@ -1,4 +1,5 @@
-{ cabal-install, haskell, jq, lib, runCommand, stableHackageDb, utillinux }:
+{ cabal-install, cabalField, dropWhile, fail, haskell, jq, lib, reverse,
+  runCommand, stableHackageDb, stringAsList, utillinux }:
 
 with lib;
 
@@ -7,64 +8,73 @@ with lib;
   extra-sources   ? [],
   hackageContents ? stableHackageDb,
   name            ? "pkg",
-  pkgs
+  ghc
 }:
 
-import (runCommand "haskell-${name}-deps.nix"
-  {
-    inherit dir hackageContents;
-    buildInputs  = [ cabal-install jq pkgs.ghc utillinux ];
-    extraSources = concatStringsSep "\n" extra-sources;
-    expr         = ''
-      with builtins;
-      listToAttrs (map (nv: { name = elemAt nv 0; value = elemAt nv 1; })
-                       (fromJSON (readFile ./versions.json)))
-    '';
-  }
-''
-  set -e
-  set -o pipefail
+with rec {
+  deps = import (runCommand "haskell-${name}-deps.nix"
+    {
+      inherit dir hackageContents;
+      buildInputs  = [ cabal-install fail ghc jq utillinux ];
+      extraSources = concatStringsSep "\n" extra-sources;
+    }
+    ''
+      set -e
+      set -o pipefail
 
-  cp -r "$dir" ./src
-  chmod +w -R ./src
+      cp -r "$dir" ./src
+      chmod +w -R ./src
 
-  export HOME="$PWD/cache"
-  mkdir -p "$HOME"/.cabal/packages/hackage.haskell.org
-  ln -s "$hackageContents"/.cabal/packages/hackage.haskell.org/00-index.tar \
-                   "$HOME"/.cabal/packages/hackage.haskell.org/
+      export HOME="$PWD/cache"
+      mkdir -p "$HOME"/.cabal/packages/hackage.haskell.org
+      ln -s "$hackageContents"/.cabal/packages/hackage.haskell.org/00-index.tar \
+                       "$HOME"/.cabal/packages/hackage.haskell.org/
 
-  cd ./src
-  cabal sandbox init
-  while read -r SRC
-  do
-    cabal sandbox add-source "$SRC"
-  done < <(echo "$extraSources")
+      cd ./src
+      cabal sandbox init
+      while read -r SRC
+      do
+        cabal sandbox add-source --snapshot "$SRC"
+      done < <(echo "$extraSources")
 
-  GOT=$(cabal install --dry-run           \
-                      --enable-tests      \
-                      --enable-benchmarks) ||
-  {
-    echo "$GOT" 1>&2
-    echo "Error listing cabal dependencies" 1>&2
-    exit 1
-  }
+      GOT=$(cabal install --dry-run           \
+                          --enable-tests      \
+                          --enable-benchmarks) || {
+        echo "$GOT" 1>&2
+        echo "Error listing cabal dependencies" 1>&2
+        exit 1
+      }
 
-  MSG='the following would be installed'
-  L=$(echo "$GOT" | grep -A 9999999 "$MSG" | tail -n+2 | tr -d ' ')
+      MSG='the following would be installed'
+      L=$(echo "$GOT" | grep -A 9999999 "$MSG" | tail -n+2 | tr -d ' ' |
+          cut -d '(' -f1)
 
-  function format {
-    while read -r DEP
-    do
-      NAME=$(echo "$DEP" | rev | cut -d '-' -f2- | rev)
-      echo "\"$NAME\" = runCabal2nix {
-                          name = \"$NAME\";
-                          url  = \"cabal://$DEP\";
-                        };"
-    done
-  }
+      echo '['                            >  "$out"
+        echo "$L" | head -n-1 | jq -R '.' >> "$out"
+      echo ']'                            >> "$out"
+    '');
 
-  echo '['                                         >  "$out"
-    echo "$L" | head -n-1 | jq -R '"cabal://" + .' >> "$out"
-    echo "\"$dir\""                                >> "$out"
-  echo ']'                                         >> "$out"
-'')
+  extrasMap = listToAttrs (map (dir: {
+                                 name = cabalField {
+                                   inherit dir;
+                                   field = "name";
+                                 };
+                                 value = dir;
+                               })
+                               extra-sources);
+
+  # Takes 'foo-bar-1.2.3' and returns 'foo-bar'
+  removeVersion = stringAsList
+                    (chars: reverse                             # Restore order
+                              (tail                             # Drop '-'
+                                (dropWhile (c: c != "-")        # Drop up to '-'
+                                           (reverse chars))));  # Start at end
+
+  # If a dependency comes from extra-sources, use its path; otherwise prefix
+  # with "cabal://" so cabal2nix will fetch from Hackage.
+  replacedDeps = map (dep: if hasAttr (removeVersion dep) extrasMap
+                              then getAttr (removeVersion dep) extrasMap
+                              else "cabal://${dep}")
+                     deps;
+};
+replacedDeps ++ [ dir ]
