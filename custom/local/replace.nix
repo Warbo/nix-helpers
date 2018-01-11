658f76dc420a20c1c54259569c07508c268d8f30
Chris Warburton <chriswarbo@gmail.com>
2018-01-11 17:21:22 +0000

    Always get replace command from nixpkgs1609's libmysql
    It's not in some others

--- custom/local/replace.nix
+++ custom/local/replace.nix
@@ -1,7 +1,9 @@
 # Provide the 'replace' command from MySQL, which is like sed without regexen
-{ libmysql, mkBin }:
+{ nixpkgs1609, mkBin }:
+
+# libmysql doesn't have a bin attribute in nixpkgs 16.03
 
 mkBin {
   name = "replace";
-  file = "${libmysql.bin}/bin/replace";
+  file = "${nixpkgs1609.libmysql.bin}/bin/replace";
 }
