e62551ffdfba81f24d070fb66019b90f5d1fa808
Chris Warburton <chriswarbo@gmail.com>
2018-02-07 18:57:21 +0000

    Load only MariaDB, not the whole of nixpkgs

--- custom/local/replace.nix
+++ custom/local/replace.nix
@@ -1,9 +1,16 @@
-# Provide the 'replace' command from MySQL, which is like sed without regexen
-{ nixpkgs1609, mkBin }:
+# Provide the 'replace' command from MariaDB, which is like sed without regexen.
+# This command was removed in later versions of MariaDB.
+{ boost159, darwin, mkBin, repo1609, self }:
 
-# libmysql doesn't have a bin attribute in nixpkgs 16.03
-
+with {
+  mariadb = self.callPackage "${repo1609}/pkgs/servers/sql/mariadb" {
+    # These overrides are copied from all-packages.nix in repo1609
+    inherit (darwin) cctools;
+    inherit (darwin.apple_sdk.frameworks) CoreServices;
+    boost = boost159;
+  };
+};
 mkBin {
   name = "replace";
-  file = "${nixpkgs1609.libmysql.bin}/bin/replace";
+  file = "${mariadb.client.bin}/bin/replace";
 }
