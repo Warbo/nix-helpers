d2e4bc6adb6839d6e88f2e3d1383ab293fe8f2be
Chris Warburton <chriswarbo@gmail.com>
2018-04-26 13:46:06 +0100

    Add caching to runCabal2nix

--- custom/local/runCabal2nix.nix
+++ custom/local/runCabal2nix.nix
@@ -1,5 +1,5 @@
-{ cabal2nix, glibcLocales, latestGit, lib, runCmd, stable, stableHackageDb,
-  withNix }:
+{ asPath, cabal2nix, cabal2nixCache, glibcLocales, latestGit, lib, runCmd,
+  stable, stableHackageDb, withNix }:
 
 with builtins;
 with lib;
@@ -20,27 +20,51 @@ with lib;
   packageDb ? stableHackageDb
 }:
 
-runCmd "run-cabal2nix${if name == "" then "" else "-${name}"}"
-  (withNix {
-    inherit packageDb;
-    buildInputs = [ cabal2nix ];
+with rec {
+  go = runCmd "run-cabal2nix${if name == "" then "" else "-${name}"}"
+    (withNix {
+      inherit packageDb;
+      buildInputs = [ cabal2nix ];
 
-    # Otherwise cabal2nix dies for accented characters
-    LANG           = "en_US.UTF-8";
-    LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+      cacheDir = if hasPrefix "cabal://" url
+                    then cabal2nixCache
+                    else "/no-cabal2nix-cache";
 
-    # cabal2nix version 20160308 breaks for git repos by trying to import
-    # nixexprs as JSON; we work around this by fetching separately.
-    url = if hasPrefix "http" url && hasSuffix ".git" url
-             then (if stable then trace "Warning: latestGit '${url}'" else id)
-                  latestGit { inherit url; stable = { unsafeSkip = true; }; }
-             else url;
-  })
-  ''
-    set -e
-    export HOME="$PWD/home"
-    cp -r "$packageDb" ./home
-    chmod 777 ./home
+      # Otherwise cabal2nix dies for accented characters
+      LANG           = "en_US.UTF-8";
+      LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
 
-    cabal2nix ${concatStringsSep " " args} "$url" > "$out"
-  ''
+      # cabal2nix version 20160308 breaks for git repos by trying to import
+      # nixexprs as JSON; we work around this by fetching separately.
+      url = if hasPrefix "http" url && hasSuffix ".git" url
+               then (if stable then trace "Warning: latestGit '${url}'" else id)
+                    latestGit { inherit url; stable = { unsafeSkip = true; }; }
+               else url;
+    })
+    ''
+      set -e
+      export HOME="$PWD/home"
+      cp -r "$packageDb" ./home
+      chmod 777 ./home
+
+      cabal2nix ${concatStringsSep " " args} "$url" > "$out"
+
+      if [[ -e "$cacheDir" ]]
+      then
+        NAMEVER=$(echo "$url" | cut -d '/' -f 3-)
+        DEST="$cacheDir/exprs/$NAMEVER.nix"
+        [[ -e "$DEST" ]] || {
+          echo "Caching result to '$DEST'" 1>&2
+          cp "$out" "$DEST" || echo "Failed to cache, oh well" 1>&2
+          chmod 777 "$DEST" || echo "Failed to cache, oh well" 1>&2
+        }
+      fi
+    '';
+
+  nameVersion = removePrefix "cabal://" url;
+
+  existing = cabal2nixCache + "/exprs/${nameVersion}.nix";
+};
+if hasPrefix "cabal://" url && pathExists existing
+   then trace "Using cache: ${existing}" (asPath existing)
+   else go
