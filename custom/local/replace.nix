e76e8df3c3c7c14cdb85847b71b553153930637d
Chris Warburton <chriswarbo@gmail.com>
2018-05-15 17:12:51 +0100

    Move selfNamedBinaries tests into their respective package definitions

--- custom/local/replace.nix
+++ custom/local/replace.nix
@@ -2,15 +2,20 @@
 # This command was removed in later versions of MariaDB.
 { boost159, darwin, mkBin, repo1609, self }:
 
-with {
+with rec {
   mariadb = self.callPackage "${repo1609}/pkgs/servers/sql/mariadb" {
     # These overrides are copied from all-packages.nix in repo1609
     inherit (darwin) cctools;
     inherit (darwin.apple_sdk.frameworks) CoreServices;
     boost = boost159;
   };
+
+  pkg = mkBin {
+    name = "replace";
+    file = "${mariadb.client.bin}/bin/replace";
+  };
 };
-mkBin {
-  name = "replace";
-  file = "${mariadb.client.bin}/bin/replace";
+{
+  inherit   pkg;
+  tests = [ pkg ];
 }
