dc12c92b460224f16d433359625ee48b3fae22a0
Chris Warburton <chriswarbo@gmail.com>
2018-05-15 17:12:51 +0100

    Move selfNamedBinaries tests into their respective package definitions

--- custom/local/pipeToNix.nix
+++ custom/local/pipeToNix.nix
@@ -1,33 +1,37 @@
-{ attrsToDirs, withNix, wrap }:
+{ attrsToDirs, hasBinary, mkBin, withDeps, withNix }:
 
 with builtins;
-with { nixy = withNix {}; };
+with rec {
+  nixy = withNix {};
 
-attrsToDirs {
-  bin = {
-    pipeToNix = wrap {
-      name   = "pipeToNix";
-      paths  = nixy.buildInputs;
-      vars   = removeAttrs nixy [ "buildInputs" ];
-      script = ''
-        #!/usr/bin/env bash
-        set -e
+  pkg = mkBin {
+    name   = "pipeToNix";
+    paths  = nixy.buildInputs;
+    vars   = removeAttrs nixy [ "buildInputs" ];
+    script = ''
+      #!/usr/bin/env bash
+      set -e
 
-        # Dumps stdin to a temporary file, adds that file to the Nix store then
-        # deletes the temp file. If an argument is given, it's used as the file
-        # name (which Nix will prefix with a content hash).
+      # Dumps stdin to a temporary file, adds that file to the Nix store then
+      # deletes the temp file. If an argument is given, it's used as the file
+      # name (which Nix will prefix with a content hash).
 
-        NAME="piped"
-        [[ -z "$1" ]] || NAME="$1"
+      NAME="piped"
+      [[ -z "$1" ]] || NAME="$1"
 
-        SCRATCH=$(mktemp -d)
-        trap "rm -rf $SCRATCH" EXIT
+      SCRATCH=$(mktemp -d)
+      trap "rm -rf $SCRATCH" EXIT
 
-        F="$SCRATCH/$NAME"
-        cat > "$F"
+      F="$SCRATCH/$NAME"
+      cat > "$F"
 
-        nix-store --add "$F"
-      '';
-    };
+      nix-store --add "$F"
+    '';
   };
+
+  tested = withDeps [ (hasBinary pkg "pipeToNix") ] pkg;
+};
+{
+  pkg   =   tested;
+  tests = [ tested ];
 }
