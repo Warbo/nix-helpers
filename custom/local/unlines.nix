74efd05a309cfea510ebf3129e1fb152d84d9b01
Chris Warburton <chriswarbo@gmail.com>
2018-05-19 01:30:33 +0100

    Move more tests into their respective definitions

--- custom/local/unlines.nix
+++ custom/local/unlines.nix
@@ -1,3 +1,15 @@
-{}:
+{ runCommand }:
 
-builtins.concatStringsSep "\n"
+with rec {
+  f = builtins.concatStringsSep "\n";
+};
+{
+  pkg   = f;
+  tests = [
+    (runCommand "test-unlines"
+      { x = f [ "foo" "bar" ]; }
+      ''
+        echo pass > "$out"
+      '')
+  ];
+}
