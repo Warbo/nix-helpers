fad1854f414e39db5a617ac8c2735184df95c72b
Chris Warburton <chriswarbo@gmail.com>
2018-05-19 15:54:49 +0100

    Move more tests into definitions

--- custom/local/mkBin.nix
+++ custom/local/mkBin.nix
@@ -1,9 +1,34 @@
 # Shorthand for making a script via 'wrap' and installing it to a bin/ directory
 { attrsToDirs, wrap }:
 
-args: attrsToDirs {
-  bin = builtins.listToAttrs [{
-    inherit (args) name;
-    value = wrap args;
-  }];
+with rec {
+  go = args: attrsToDirs {
+    bin = builtins.listToAttrs [{
+      inherit (args) name;
+      value = wrap args;
+    }];
+  };
+};
+{
+  pkg   = go;
+  tests = [
+    (runCommand "mkBin-test"
+      {
+        buildInputs = [
+          fail
+          (go {
+            name   = "ping";
+            script = ''
+              #!/usr/bin/env bash
+              echo "pong"
+            '';
+          })
+        ];
+      }
+      ''
+        X=$(ping)
+        [[ "x$X" = "xpong" ]] || fail "Output was '$X'"
+        echo pass > "$out"
+      '')
+  ];
 }
