e5bbc364aaa2e041dc2cc6e57c6a6fa3898b7990
Chris Warburton <chriswarbo@gmail.com>
2018-05-19 15:54:49 +0100

    Move more tests into definitions

--- custom/local/isPath.nix
+++ custom/local/isPath.nix
@@ -1,6 +1,32 @@
-{ lib }:
+{ dummyWithEnv, lib }:
 
 with builtins;
 with lib;
-
-x: typeOf x == "path" || (typeOf x == "string" && hasPrefix "/" x)
+with {
+  go = x: typeOf x == "path" || (typeOf x == "string" && hasPrefix "/" x);
+};
+{
+  pkg   = go;
+  tests = [
+    (dummyWithEnv {
+      name  = "relativePathIsPath";
+      value = isPath ./isPath.nix;
+    })
+    (dummyWithEnv {
+      name  = "absolutePathIsPath";
+      value = isPath /tmp;
+    })
+    (dummyWithEnv {
+      name  = "pathStringIsPath";
+      value = isPath "/tmp";
+    })
+    (dummyWithEnv {
+      name  = "stringIsNotPath";
+      value = !(isPath "foo");
+    })
+    (dummyWithEnv {
+      name  = "otherIsNotPath";
+      value = !(isPath 42);
+    })
+  ];
+}
