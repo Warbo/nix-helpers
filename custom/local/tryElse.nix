4875a78e256acd5e07a18835504bf8ac23b66734
Chris Warburton <chriswarbo@gmail.com>
2018-05-19 01:30:33 +0100

    Move more tests into their respective definitions

--- custom/local/tryElse.nix
+++ custom/local/tryElse.nix
@@ -1,5 +1,17 @@
 {}:
 
-prefer: fallback: if (builtins.tryEval prefer).success
-                     then prefer
-                     else fallback
+with rec {
+  f = prefer: fallback: if (builtins.tryEval prefer).success
+                           then prefer
+                           else fallback;
+};
+{
+  pkg   = f;
+  tests = [
+    (runCommand "test-tryElse"
+      { x = tryElse <nope> "fallback"; }
+      ''
+        echo pass > "$out"
+      '')
+  ];
+}
