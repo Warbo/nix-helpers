e3c023b4fb300f42bb4c0cbb9ea0d451f3224e52
Chris Warburton <chriswarbo@gmail.com>
2018-05-20 02:59:20 +0100

    Move more tests into their definitions

--- custom/local/cabalField.nix
+++ custom/local/cabalField.nix
@@ -1,32 +1,50 @@
-{ fail, runCommand }:
+{ fail, haskellPackages, runCommand, unpack }:
 
 with builtins;
-{ dir, field }:
-  import (runCommand "cabal-field-${field}.nix"
-           {
-             inherit dir field;
-             buildInputs = [ fail ];
-           }
-           ''
-             set   -e
-             set   -o pipefail
-             shopt -s nullglob
-
-             cd "$dir" || fail "Couldn't cd to '$dir'"
-
-             VAL=""
-             for F in *.cabal
-             do
-               LINES=$(grep -i "^$field\s*:" < "$F") ||
-                 fail "No '$field' lines in '$F'"
-
-               VAL=$(echo "$LINES" | head -n1       |
-                                     cut -d ':' -f2 |
-                                     sed -e 's/\s//g')
-             done
-             [[ -n "$VAL" ]] || {
-               echo "Couldn't find Cabal field '$field' in '$dir'" 1>&2
-               exit 1
+rec {
+  pkg = { dir, field }:
+    import (runCommand "cabal-field-${field}.nix"
+             {
+               inherit dir field;
+               buildInputs = [ fail ];
              }
-             echo "\"$VAL\"" > "$out"
-           '')
+             ''
+               set   -e
+               set   -o pipefail
+               shopt -s nullglob
+
+               cd "$dir" || fail "Couldn't cd to '$dir'"
+
+               VAL=""
+               for F in *.cabal
+               do
+                 LINES=$(grep -i "^$field\s*:" < "$F") ||
+                   fail "No '$field' lines in '$F'"
+
+                 VAL=$(echo "$LINES" | head -n1       |
+                                       cut -d ':' -f2 |
+                                       sed -e 's/\s//g')
+               done
+               [[ -n "$VAL" ]] || {
+                 echo "Couldn't find Cabal field '$field' in '$dir'" 1>&2
+                 exit 1
+               }
+               echo "\"$VAL\"" > "$out"
+             '');
+  tests = [
+    (runCommand "cabalField-test"
+      {
+        found = pkg {
+          dir   = unpack haskellPackages.text.src;
+          field = "name";
+        };
+      }
+      ''
+        [[ "x$found" = "xtext" ]] || {
+          echo "Got '$found' instead of 'text'" 1>&2
+          exit 1
+        }
+        mkdir "$out"
+      '')
+  ];
+}
