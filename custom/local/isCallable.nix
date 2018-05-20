ac61dcd8f79a9d08c8bf4fca50c2a6e3cd555233
Chris Warburton <chriswarbo@gmail.com>
2018-05-20 02:59:20 +0100

    Move more tests into their definitions

--- custom/local/isCallable.nix
+++ custom/local/isCallable.nix
@@ -1,4 +1,10 @@
-{}:
+{ callPackage, nothing }:
 
 with builtins;
-x: isFunction x || (isAttrs x && x ? __functor)
+rec {
+  pkg   = x: isFunction x || (isAttrs x && x ? __functor);
+  tests = [
+    (assert pkg (callPackage ({}: (x: abort "shouldn't force")) {});
+     nothing)
+  ];
+}
