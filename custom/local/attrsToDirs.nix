77592864e2592109b15d4f8f10f6419296b3a60f
Chris Warburton <chriswarbo@gmail.com>
2018-05-20 02:59:20 +0100

    Move more tests into their definitions

--- custom/local/attrsToDirs.nix
+++ custom/local/attrsToDirs.nix
@@ -16,43 +16,47 @@ with rec {
                               (attrNames val)
                else abort "Unsupported type ${typeOf val} in attrsToDirs";
 };
+rec {
+  pkg = attrs:
+    with rec {
+      # We can't have empty attr names, so always stick a dummy '_' at the start,
+      # and strip it off in the build script
+      data = listToAttrs (toPaths "_" attrs);
 
-attrs:
-  with rec {
-    # We can't have empty attr names, so always stick a dummy '_' at the start,
-    # and strip it off in the build script
-    data = listToAttrs (toPaths "_" attrs);
+      # The element order of these lists is arbitrary, but they must match
+      paths    = attrNames data;
+      content  = map (n: getAttr n data) paths;
 
-    # The element order of these lists is arbitrary, but they must match
-    paths    = attrNames data;
-    content  = map (n: getAttr n data) paths;
+      # Create bash code and env vars for the build command, without forcing any
+      # derivations to be built at eval time.
+      pathData    = nixListToBashArray { name = "VALPATHS"; args = paths;   };
+      contentData = nixListToBashArray { name = "VALUES";   args = content; };
+    };
+    if attrs == {}
+       then nothing
+       else runCmd "merged" (pathData.env // contentData.env) ''
+              ${pathData.code}
+              ${contentData.code}
 
-    # Create bash code and env vars for the build command, without forcing any
-    # derivations to be built at eval time.
-    pathData    = nixListToBashArray { name = "VALPATHS"; args = paths;   };
-    contentData = nixListToBashArray { name = "VALUES";   args = content; };
-  };
-  if attrs == {}
-     then nothing
-     else runCmd "merged" (pathData.env // contentData.env) ''
-            ${pathData.code}
-            ${contentData.code}
+              # Loop over each path/content entry in our arrays (off-by-one cruft
+              # is due to seq preferring to count from 1)
+              for NPLUSONE in $(seq 1 "''${#VALUES[@]}")
+              do
+                N=$(( NPLUSONE - 1 ))
 
-            # Loop over each path/content entry in our arrays (off-by-one cruft
-            # is due to seq preferring to count from 1)
-            for NPLUSONE in $(seq 1 "''${#VALUES[@]}")
-            do
-              N=$(( NPLUSONE - 1 ))
+                # The output path we're going to make; cut off leading _ and
+                # prepend $out
+                STRIPPED=$(echo "''${VALPATHS[$N]}" | cut -c 2-)
+                       P="$out$STRIPPED"
 
-              # The output path we're going to make; cut off leading _ and
-              # prepend $out
-              STRIPPED=$(echo "''${VALPATHS[$N]}" | cut -c 2-)
-                     P="$out$STRIPPED"
+                # Ensure parent directories exist
+                mkdir -p "$(dirname "$P")"
 
-              # Ensure parent directories exist
-              mkdir -p "$(dirname "$P")"
-
-              # Link value in place (saves space compared to copying)
-              ln -s "''${VALUES[$N]}" "$P"
-            done
-          ''
+                # Link value in place (saves space compared to copying)
+                ln -s "''${VALUES[$N]}" "$P"
+              done
+            '';
+  tests = [
+    (pkg { foo = { bar = ./attrsToDirs.nix; }; })
+  ];
+}
