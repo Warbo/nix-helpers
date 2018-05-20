2a31fcf1bc5dcb634521191309f6b5fddab6361e
Chris Warburton <chriswarbo@gmail.com>
2018-05-20 02:59:49 +0100

    Do syntax check and variable occurrence check

--- check.sh
+++ check.sh
@@ -4,8 +4,6 @@ set -e
 # Simple, quick sanity check. Useful as a git pre-commit hook.
 find . -name "*.nix" | while read -r F
 do
-    P=$(readlink -f "$F")
-    echo "Checking '$P'" 1>&2
-    nix-instantiate --show-trace --read-write-mode --eval \
-                    -E "with builtins; typeOf (import $P)" > /dev/null
+    echo "Checking '$F'" 1>&2
+    nix-instantiate --parse "$F" > /dev/null
 done
