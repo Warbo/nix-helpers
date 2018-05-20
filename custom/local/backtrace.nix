01273810fc6ad00b11b347ac9400fed53322624a
Chris Warburton <chriswarbo@gmail.com>
2018-05-20 02:59:20 +0100

    Move more tests into their definitions

--- custom/local/backtrace.nix
+++ custom/local/backtrace.nix
@@ -1,28 +1,49 @@
 # Provides a 'backtrace' command showing the process hierarchy of its caller
-{ mkBin }:
+{ mkBin, runCommand }:
 
-mkBin {
-  name   = "backtrace";
-  script = ''
-    #!/usr/bin/env bash
-    set -e
+rec {
+  pkg = mkBin {
+    name   = "backtrace";
+    script = ''
+      #!/usr/bin/env bash
+      set -e
 
-    [[ -z "$NOTRACE" ]] || exit 0
+      [[ -z "$NOTRACE" ]] || exit 0
 
-    echo "Begin Backtrace:"
+      echo "Begin Backtrace:"
 
-    ID="$$"  # Current PID
-    while [[ "$ID" -gt 1 ]]  # Loop until we reach init
-    do
-      # Show this PID's
-      cat "/proc/$ID/cmdline" | tr '\0' ' '
-      echo
+      ID="$$"  # Current PID
+      while [[ "$ID" -gt 1 ]]  # Loop until we reach init
+      do
+        # Show this PID's
+        cat "/proc/$ID/cmdline" | tr '\0' ' '
+        echo
 
-      # Get parent's PID
-      ID=$(grep PPid < "/proc/$ID/status" | cut -d ':' -f2  |
-                                            sed -e 's/\s//g')
-    done
+        # Get parent's PID
+        ID=$(grep PPid < "/proc/$ID/status" | cut -d ':' -f2  |
+                                              sed -e 's/\s//g')
+      done
 
-    echo "End Backtrace"
-  '';
+      echo "End Backtrace"
+    '';
+  };
+  tests = [
+    (runCommand "backtrace-test"
+      { buildInputs = [ pkg ]; }
+      ''
+        X=$(NOTRACE=1 backtrace)
+        [[ -z "$X" ]] || {
+          echo "NOTRACE should suppress trace" 1>&2
+          exit 1
+        }
+
+        Y=$(backtrace)
+        for Z in "Backtrace" "End Backtrace" "bash"
+        do
+          echo "$Y" | grep -F "$Z" || fail "Didn't find '$Z'"
+        done
+
+        echo pass > "$out"
+      '')
+  ];
 }
