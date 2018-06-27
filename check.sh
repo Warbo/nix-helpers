49bf375d5993475b9b798984db800253500c1fba
Chris Warburton <chriswarbo@gmail.com>
2018-06-27 18:16:58 +0100

    Make check.sh evaluate test derivations
    This ensures that all files can be evaluated (assuming that our import mechanism
    doesn't break, but then 'allDrvsIn' is unlikely to work!) and it ensures that
    any Nix-level assertions made in the tests get exercised.

--- check.sh
+++ check.sh
@@ -2,8 +2,16 @@
 set -e
 
 # Simple, quick sanity check. Useful as a git pre-commit hook.
+
 find . -name "*.nix" | while read -r F
 do
-    echo "Checking '$F'" 1>&2
+    echo "Checking syntax of '$F'" 1>&2
     nix-instantiate --parse "$F" > /dev/null
 done
+
+echo "Checking we can evaluate all test derivations" 1>&2
+nix-instantiate --show-trace \
+                -E 'with import ./.; allDrvsIn nix-helpers-tests' || {
+    echo "Couldn't evaluate all test derivations" 1>&2
+    exit 1
+}
