d012fd6326a96f9c0067edab93970bc72bf4a672
Chris Warburton <chriswarbo@gmail.com>
2018-07-14 00:54:26 +0100

    Alter haskellRelease to allow some combos to be prevented
    We were defining nixpkgs sets and hackage sets, unconditionally, for all combos
    given. Now we take in the combos which should use hackage, those which should
    use nixpkgs, and any custom sets.

--- helpers/haskellRelease.nix
+++ helpers/haskellRelease.nix
@@ -9,11 +9,22 @@ with rec {
   getNix = v: getAttr v pinnedNixpkgs;
 
   # Make the nixVersion attr (if kept) a set of all its Haskell versions
-  buildForNixpkgs = haskellVersions: hs: nixVersion:
+  buildForNixpkgs = hs: nixVersion: haskellVersions:
     assert isString nixVersion || die {
       error = "nixVersion should be string";
       given = typeOf nixVersion;
     };
+    assert isList haskellVersions && all isString haskellVersions || die {
+      error = "haskellVersions should be list of strings";
+      given = typeOf haskellVersions;
+      elems = if isList haskellVerisons
+                 then map typeOf haskellVersions
+                 else "Not a list";
+    };
+    assert isFunction hs || die ({
+      error = "hs should be a function";
+      given = typeOf hs;
+    } // (if isAttrs hs then { names = attrNames hs; } else {}));
     with {
       nixpkgs = getNix nixVersion // {
         # Avoid https://github.com/haskell/zlib/issues/11
@@ -22,77 +33,100 @@ with rec {
                  (repo1609 + "/pkgs/development/libraries/zlib") {};
       };
     };
-    with nixpkgs.haskell;
     genAttrs haskellVersions
-             (name: hs nixpkgs (getAttr name nixpkgs.haskell.packages));
+             (name: hs {
+               inherit nixpkgs;
+               haskellPackages = getAttr name nixpkgs.haskell.packages;
+             });
 
-  # Defines nixpkgs and hackagedb uilds, using a given haskellPackages set
-  pkgExpr = { dir, name }: nixpkgs: haskellPackages:
+  # Calls the Haskell package defined by the given file with dummy
+  # arguments, to see which arguments should come from nixpkgs and which
+  # from haskellPackages (self). Uses this info to call the package
+  # "properly". This is especially useful for args like 'zlib', which
+  # could be from either.
+  callProperly = nixpkgs: self: file:
+    assert isAttrs nixpkgs;
+    assert isAttrs self;
     with rec {
-      # Bare function, which we'll give arguments from the nixpkgs Haskell set
-      nixpkgsDeps = runCabal2nix { url = dir; };
+      func    = import file;
+      args    = attrNames (functionArgs func);
+      dummies = listToAttrs (map (x: { name = x; value = x; }) args);
+      sysArgs = func (dummies // {
+        mkDerivation = args: args.librarySystemDepends or [];
+      });
+      sysPkgs = listToAttrs
+        (map (name: { inherit name; value = getAttr name nixpkgs; })
+        sysArgs);
+    };
+    self.callPackage func sysPkgs;
 
+  buildForHackage = { dir, name }: { haskellPackages, nixpkgs }:
+    assert isString name;
+    assert isAttrs haskellPackages;
+    assert isAttrs nixpkgs;
+    assert !isDerivation haskellPackages;
+    assert !isDerivation nixpkgs;
+    with rec {
       # Uses a Cabal sandbox to pick dependencies from (a snapshot of) Hackage
       hackageDeps = haskellPkgDeps {
         inherit dir;
         inherit (haskellPackages) ghc;
       };
 
-      # Calls the Haskell package defined by the given file with dummy
-      # arguments, to see which arguments should come from nixpkgs and which
-      # from haskellPackages (self). Uses this info to call the package
-      # "properly". This is especially useful for args like 'zlib', which
-      # could be from either.
-      callProperly = self: file:
-        with rec {
-          func    = import file;
-          args    = attrNames (functionArgs func);
-          dummies = listToAttrs (map (x: { name = x; value = x; }) args);
-          sysArgs = func (dummies // {
-            mkDerivation = args: args.librarySystemDepends or [];
-          });
-          sysPkgs = listToAttrs
-            (map (name: { inherit name; value = getAttr name nixpkgs; })
-                 sysArgs);
-        };
-        self.callPackage func sysPkgs;
-
       hsPkgs = haskellPackages.override {
-        overrides = self: super: mapAttrs (_: callProperly self)
-                                          hackageDeps;
+        overrides = self: super: mapAttrs (_: callProperly nixpkgs self)
+          hackageDeps;
       };
     };
-    {
-      hackageDeps = getAttr name hsPkgs;
-      nixpkgsDeps = callProperly haskellPackages nixpkgsDeps;
-    };
+    getAttr name hsPkgs;
+
+  buildForHaskell = { dir, name }: { haskellPackages, nixpkgs }:
+    callProperly nixpkgs haskellPackages (runCabal2nix { url = dir; });
 };
 rec {
   def = {
-    dir,                # Directory of a Haskell project
-    extraSets  ? {},    # Extra Haskell package sets to use (e.g. overridden)
-    haskellVersions,    # List of haskell.packages entries to use
-    name,               # Cabal package name
-    nixpkgsVersions     # List of nixpkgs versions to use
+    dir,               # Directory of a Haskell project
+    customSets  ? {},  # Extra Haskell package sets to use (e.g. overridden)
+    hackageSets ? {},  # Sets to use with Hackage dependencies
+    name,              # Cabal package name
+    nixpkgsSets ? {},  # Sets to use with nixpkgs dependencies
   }:
-    assert (isList nixpkgsVersions && all isString nixpkgsVersions) || die {
-      error        = "nixpkgsVersions should be a list of strings";
-      actualType   = typeOf nixpkgsVersions;
-      elementTypes = if isList nixpkgsVersions
-                        then map typeOf nixpkgsVersions
-                        else "Not a list";
+    with {
+      forHackage = buildForHackage { inherit dir name; };
+      forHaskell = buildForHaskell { inherit dir name; };
+
+      isSetOf = valType: name: x:
+        (!isDerivation           x &&
+         isAttrs                 x &&
+         all (valType (name + " value")) (attrValues x)) || die {
+          error = name + " should be a set";
+          given = if isDerivation x then "derivation" else typeOf x;
+        };
+      isListOf = elemType: name: x:
+        (isList x && all (elemType (name + " element")) x) || die {
+          error = name + " should be a list";
+          given = typeOf x;
+        };
+      isASet = name: x: (!isDerivation x && isAttrs x) || die {
+        error = name + " should be a set";
+        given = if isDerivation x then "derivation" else typeOf x;
+      };
+      isAString = name: x: isString x || die {
+        error = name + " should be a string";
+        given = typeOf x;
+      };
     };
-    assert (isList haskellVersions && all isString haskellVersions) || die {
-      error        = "haskellVersions should be a list of strings";
-      actualType   = typeOf haskellVersions;
-      elementTypes = if isList haskellVersions
-                        then map typeOf haskellVersions
-                        else "Not a list";
-    };
-    # Defines builds for Haskell versions for nixpkgs versions, plus extras
-    with { buildForHaskell = pkgExpr { inherit dir name; }; };
-    genAttrs nixpkgsVersions
-             (buildForNixpkgs haskellVersions buildForHaskell);
+    assert isSetOf isASet               "customSets"  customSets;
+    assert isSetOf (isListOf isAString) "hackageSets" hackageSets;
+    assert isSetOf (isListOf isAString) "nixpkgsSets" nixpkgsSets;
+    fold mergeAttrs {} [
+      (if  customSets == {} then {} else {
+        customDeps  = mapAttrs                  forHaskell   customSets; })
+      (if hackageSets == {} then {} else {
+        hackageDeps = mapAttrs (buildForNixpkgs forHackage) hackageSets; })
+      (if nixpkgsSets == {} then {} else {
+        nixpkgsDeps = mapAttrs (buildForNixpkgs forHaskell) nixpkgsSets; })
+    ];
 
   tests =
     # Check that this system works for some common, and some problematic, Haskell
@@ -111,9 +145,9 @@ rec {
 
       getResult = name: def {
         inherit name;
-        dir             = unpack (getPkg name).src;
-        haskellVersions = [      hsVersion ];
-        nixpkgsVersions = [ currentVersion ];
+        dir         = unpack (getPkg name).src;
+        nixpkgsSets = { "${currentVersion}" = [ hsVersion ]; };
+        hackageSets = { "${currentVersion}" = [ hsVersion ]; };
       };
 
       check = name:
@@ -126,18 +160,25 @@ rec {
         assert isAttrs result || die {
           error = "Generated release doesn't define a set";
         };
-        assert attrNames result == [ stable ] || die {
-          error = "Should have one attribute, with stable name '${stable}'";
+        assert elem "hackageDeps" (attrNames result) &&
+               elem "nixpkgsDeps" (attrNames result) &&
+               length (attrNames result) == 2        || die {
+          error = "Attributes should be hackageDeps and nixpkgsDeps";
           names = attrNames result;
         };
-        assert attrNames result."${stable}" == [ hsVersion ] || die {
-          error       = "Set should have one GHC version, namely '${hsVersion}'";
-          haskellSets = attrNames result."${stable}";
-          resultSets  = attrNames result;
+        assert attrNames result.hackageDeps == [ stable ] || die {
+          error = "hackageDeps should only contain '${stable}'";
+          names = attrNames result.hackageDeps;
         };
-        {
-          inherit (result."${stable}"."${hsVersion}") hackageDeps nixpkgsDeps;
+        assert attrNames result.nixpkgsDeps == [ stable ] || die {
+          error = "nixpkgsDeps should only contain '${stable}'";
+          names = attrNames result.nixpkgsDeps;
         };
+        assert attrNames result.hackageDeps."${stable}" == [hsVersion] || die {
+          error = "hackageDeps.${stable} should only contain '${hsVersion}'";
+          names = attrNames result.hackageDeps."${stable}";
+        };
+        result;
     };
     {
       # A widely-used Haskell package, see if it works
