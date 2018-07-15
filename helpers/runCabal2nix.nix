43b5cb85ecea14487cca3eebd58ab9967903ebe4
Chris Warburton <chriswarbo@gmail.com>
2018-07-15 14:46:48 +0100

    Refactor runCabal2nix to allow tests

--- helpers/runCabal2nix.nix
+++ helpers/runCabal2nix.nix
@@ -1,81 +1,85 @@
 { asPath, cabal2nix, cabal2nixCache, glibcLocales, latestGit, lib,
-  pinnedCabal2nix ? cabal2nix, runCmd, stableHackageDb, withNix }:
+  pinnedCabal2nix ? cabal2nix, runCmd, stableHackageDb, withArgsOf, withNix }:
 
 with builtins;
 with lib;
-
-{
-  name ? "",
-
-  # Package location, containing a .cabal file, e.g. path, git repo, "cabal://"
-  url,
-
-  # Extra arguments for cabal2nix command. NOTE: you must quote things yourself,
-  # if needed. Also, the available options may vary between cabal2nix versions.
-  args ? [],
-
-  # Hackage repo contents to use. Get the latest content (via 'cabal update') by
-  # using hackageDb, but this doesn't cache well. Defaults to stableHackageDb,
-  # which is built from a fixed git revision and hence caches well.
-  packageDb ? stableHackageDb
-}:
-
 with rec {
-  go = runCmd "run-cabal2nix${if name == "" then "" else "-${name}"}"
-    (withNix {
-      inherit packageDb;
-      buildInputs = [ pinnedCabal2nix ];
-
-      cacheDir = if hasPrefix "cabal://" url
-                    then cabal2nixCache
-                    else "/no-cabal2nix-cache";
-
-      # Otherwise cabal2nix dies for accented characters
-      LANG           = "en_US.UTF-8";
-      LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
-
-      # cabal2nix version 20160308 breaks for git repos by trying to import
-      # nixexprs as JSON; we work around this by fetching separately.
-      url = if hasPrefix "http" url && hasSuffix ".git" url
-               then trace "Warning: latestGit '${url}'" latestGit {
-                      inherit url;
-                      stable = { unsafeSkip = true; };
-                    }
-               else url;
-    })
-    ''
-      set -e
-      export HOME="$PWD/home"
-      cp -r "$packageDb" ./home
-      chmod 777 ./home
-
-      cabal2nix ${concatStringsSep " " args} "$url" > "$out"
-
-      if [[ -e "$cacheDir" ]]
-      then
-        NAMEVER=$(echo "$url" | cut -d '/' -f 3-)
-        DEST="$cacheDir/exprs/$NAMEVER.nix"
-        [[ -e "$DEST" ]] || {
-          echo "Caching result to '$DEST'" 1>&2
-          cp "$out" "$DEST" || echo "Failed to cache, oh well" 1>&2
-          chmod 777 "$DEST" || echo "Failed to cache, oh well" 1>&2
-        }
-      fi
+  warn = url: trace (toJSON {
+    inherit url;
+    warning  = "deprecated";
+    function = "runCabal2nix";
+    message  = ''
+      runCabal2nix is deprecated, since nixpkgs 18.03 includes equivalent
+      functionality such as callHackage, hackage2nix and haskellSrc2nix.
     '';
+  });
 
-  nameVersion = removePrefix "cabal://" url;
+  # Sets up the environment for running cabal2nix (lots of pettiness)
+  env = url: warn url withNix {
+    buildInputs = [ pinnedCabal2nix ];
 
-  existing = cabal2nixCache + "/exprs/${nameVersion}.nix";
+    # Otherwise cabal2nix dies for accented characters
+    LANG           = "en_US.UTF-8";
+    LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+
+    cacheDir = if hasPrefix "cabal://" url
+                  then cabal2nixCache
+                  else "/no-cabal2nix-cache";
+
+    # cabal2nix version 20160308 breaks for git repos by trying to import
+    # nixexprs as JSON; we work around this by fetching separately.
+    url = if hasPrefix "http" url && hasSuffix ".git" url
+             then trace "Warning: latestGit '${url}'" latestGit {
+                    inherit url;
+                    stable = { unsafeSkip = true; };
+                  }
+             else url;
+  };
+
+  # This is the actual implementation which runs cabal2nix and (possibly) caches
+  go =
+    {
+      name ? "unknown",
+
+      # Package containing a .cabal file, e.g. path, git repo, "cabal://"
+      url,
+
+      # Extra arguments for cabal2nix command. NOTE: Quote things yourself if
+      # needed. Also, the available options may vary between cabal2nix versions.
+      args ? [],
+
+      # Hackage contents to use. Get the latest content (via 'cabal update') by
+      # using hackageDb, but that doesn't cache well. Defaults to
+      # stableHackageDb, which does since it's built from a fixed git rev.
+      packageDb ? stableHackageDb
+    }: runCmd "run-cabal2nix-${name}" (env url // { inherit packageDb; }) ''
+         set -e
+         export HOME="$PWD/home"
+         cp -r "$packageDb" ./home
+         chmod 777 ./home
+
+         cabal2nix ${concatStringsSep " " args} "$url" > "$out"
+
+         if [[ -e "$cacheDir" ]]
+         then
+           NAMEVER=$(echo "$url" | cut -d '/' -f 3-)
+           DEST="$cacheDir/exprs/$NAMEVER.nix"
+           [[ -e "$DEST" ]] || {
+             echo "Caching result to '$DEST'" 1>&2
+             cp "$out" "$DEST" || echo "Failed to cache, oh well" 1>&2
+             chmod 777 "$DEST" || echo "Failed to cache, oh well" 1>&2
+           }
+         fi
+       '';
+
+  # Looks up from the cache if available, otherwise builds (and caches)
+  cached = { url, ... }@args:
+    with { c = cabal2nixCache + "/exprs/${removePrefix "cabal://" url}.nix"; };
+    if hasPrefix "cabal://" url && pathExists c
+       then trace "Using cache: ${c}" (asPath c)
+       else go args;
 };
-assert trace (toJSON {
-  inherit url;
-  warning  = "deprecated";
-  function = "runCabal2nix";
-  message  = ''
-    runCabal2nix is deprecated, since nixpkgs 18.03 includes equivalent
-    functionality such as callHackage, hackage2nix and haskellSrc2nix.
-  '';
-}) true;
-if hasPrefix "cabal://" url && pathExists existing
-   then trace "Using cache: ${existing}" (asPath existing)
-   else go
+{
+  def   = withArgsOf go cached;
+  tests = {};
+}
