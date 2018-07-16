f273d44b8e6901e0809c1769889cf26b7070f94e
Chris Warburton <chriswarbo@gmail.com>
2018-07-16 19:37:35 +0100

    Remove eval from check.sh since it's so slow

--- check.sh
+++ check.sh
@@ -8,10 +8,3 @@ do
     echo "Checking syntax of '$F'" 1>&2
     nix-instantiate --parse "$F" > /dev/null
 done
-
-echo "Checking we can evaluate all test derivations" 1>&2
-nix-instantiate --show-trace \
-                -E 'with import ./.; allDrvsIn nix-helpers-tests' || {
-    echo "Couldn't evaluate all test derivations" 1>&2
-    exit 1
-}
