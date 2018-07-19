31b9eb8366ba569cf83ba36b198c6abe6d81663f
Chris Warburton <chriswarbo@gmail.com>
2018-07-19 03:36:13 +0100

    Test zlib properly

--- helpers/haskellRelease.nix
+++ helpers/haskellRelease.nix
@@ -247,12 +247,12 @@ rec {
       text = check { name = "text"; };
 
       # zlib is awkward, since it's both a Haskell package and a system package
-      zlib = check { name = "zlib"; hackageSets = {}; nixpkgsSets = {}; };
+      zlib = check { name = "zlib"; nixpkgsSets = {}; };
 
       # digest also depends on the system's zlib
       digest = check { name = "digest"; };
 
       # This depends on the Haskell zlib package, rather than the system one
-      zlib-bindings = check { name = "zlib-bindings"; hackageSets = {}; };
+      zlib-bindings = check { name = "zlib-bindings"; nixpkgsSets = {}; };
     };
 }
