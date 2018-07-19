87f3f44935bd79c47672ab4cc8c4c4d7e4d0f493
Chris Warburton <chriswarbo@gmail.com>
2018-07-19 10:25:42 +0100

    Break bytestring <-> test-framework cycle

--- helpers/haskellTestCycles.nix
+++ helpers/haskellTestCycles.nix
@@ -1,12 +1,17 @@
+# List Haskell package names whose test dependencies are cyclic. For example, a
+# 'foo' library may depend on 'bar', and the test suite of 'bar' may depend on
+# 'foo'. Naively this is cyclic, but if we put 'bar' in this list its tests
+# suite will be skipped and the cycle will be broken.
 {}:
 
 {
   def   = [
-    "clock"
+    "bytestring" # bytestring <-> test-framework
+    "clock"      # clock <-> tasty
     "containers"
-    "deepseq"
-    "pretty" # QuickCheck -> template-haskell -> pretty; pretty -> QuickCheck
-    "time"
+    "deepseq"    # deepseq <-> HUnit
+    "pretty"     # QuickCheck -> template-haskell -> pretty -> QuickCheck
+    "time"       # time <-> test-framework
   ];
   tests = {};
 }
