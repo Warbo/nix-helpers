0f5b5c6033cf0057d5e4847abf02250736dc89af
Chris Warburton <chriswarbo@gmail.com>
2019-01-30 13:46:30 +0000

    Add in-debian-chroot

---
+++ helpers/in-debian-chroot.nix
@@ -0,0 +1,106 @@
+# Run a command in a Debian chroot which has the given packages installed
+{ bash, die, fetchurl, getType, lib, proot, runCommand, wrap, writeScript }:
+
+with builtins;
+with lib;
+with rec {
+  rootVersion = "67a0101a76eed558d4b61a484a27c9f9d7a119f4/stretch";
+
+  rootRepo = "debuerreotype/docker-debian-artifacts";
+
+  rootfs = fetchurl {
+    url    = "https://github.com/${rootRepo}/raw/${rootVersion}/rootfs.tar.xz";
+    sha256 = "1ff2qjvfj6fbwwj17wgmn3a4mlka1xv1p3jyj465dbf4qf3x0ijm";
+  };
+
+  # See https://github.com/proot-me/PRoot/issues/106
+  PROOT_NO_SECCOMP = "1";
+
+  env = { rootfs, pkgs ? [], debs ? [] }: runCommand "debian-chroot"
+    {
+      inherit rootfs PROOT_NO_SECCOMP;
+      buildInputs      = [ proot ];
+      SSL_CERT_FILE    = /etc/ssl/certs/ca-bundle.crt;
+      script           = writeScript "setup.sh" ''
+        #!/usr/bin/env bash
+        set -e
+        apt-get update
+        ${concatStringsSep "\n" (map (p: "apt-get install -y ${p}") pkgs)}
+        while read -r P
+        do
+          dpkg -i "$P"
+        done < <(find /root -maxdepth 1 -type f -name '*.deb' | sort -n)
+      '';
+    }
+    ''
+      echo "Unpacking Debian" 1>&2
+      mkdir "$out"
+      pushd "$out"
+        tar xf "$rootfs"
+      popd
+
+      echo "Installing setup script" 1>&2
+      cp "$script" "$out/setup.sh"
+
+      echo "Pointing PATH to binary locations" 1>&2
+      export PATH="/bin:/usr/bin:/sbin:/usr/sbin:$PATH"
+
+      echo "Resetting /tmp variables" 1>&2
+      export TMPDIR=/tmp
+      export TEMPDIR=/tmp
+      export TMP=/tmp
+      export TEMP=/tmp
+
+      if [[ ${toString (length debs)} -gt 0 ]]
+      then
+        echo "Copying across .debs" 1>&2
+        COUNT=0
+        ${concatStringsSep "\n" (map (f: ''
+          cp "${f}" "$out/root/$COUNT.deb"
+          COUNT=$(( COUNT + 1 ))
+        '') debs)}
+      fi
+
+      echo "Setting up" 1>&2
+      proot -r "$out" -b /proc -b /dev -0 /setup.sh
+    '';
+};
+
+{ rootfs, pkgs ? [], debs ? [] }:
+
+assert isList pkgs || die {
+  error = "Expected 'pkgs' to be a list of package names";
+  type  = typeOf pkgs;
+};
+assert all isString pkgs || die {
+  error = "Expected package names in 'pkgs' to be strings";
+  types = map typeOf pkgs;
+};
+assert isList debs || die {
+  error = "Expected 'debs' to be a list of .deb files";
+  type  = typeOf debs;
+};
+assert all (f: elem (getType f) [ "derivation" "path" ]) debs || die {
+  error = "Expected each of 'debs' to be a derivation or path to a .deb file";
+  types = map getType debs;
+};
+
+wrap {
+  name   = "in-debian-chroot";
+  paths  = [ bash proot ];
+  vars   = {
+    inherit PROOT_NO_SECCOMP;
+    env = env { inherit debs rootfs pkgs; };
+  };
+  script = ''
+    #!/usr/bin/env bash
+    export PATH="/bin:/usr/bin:/sbin:/usr/sbin:$PATH"
+    export TMPDIR=/tmp
+    export TEMPDIR=/tmp
+    export TMP=/tmp
+    export TEMP=/tmp
+
+    # shellcheck disable=SC2154
+    proot -r "$env" -b /proc -b /dev -b /nix -b /tmp -b /home "$@"
+  '';
+}
