347793a956720bd7a9d08d6f6993c716e99c27cb
Chris Warburton <chriswarbo@gmail.com>
2019-05-22 15:09:45 +0100

    Fix 'without' for multi-output derivations

--- helpers/without.nix
+++ helpers/without.nix
@@ -1,29 +1,57 @@
-{ bash, coreutils, fail, hello, lib, runCommand, writeScript }:
+{ bash, coreutils, fail, hello, lib, mupdf, runCommand, writeScript }:
 
-with {
-  mkCmd = p: ''
-    [[ -e "$out" ]] && "${coreutils}/bin/rm" -rf "$out"/${lib.escapeShellArg p}
-  '';
+with lib;
+with rec {
+  makeCommand = path: output:
+    with { esc = ''"$'' + output + ''"/${escapeShellArg path}''; };
+    ''
+      if [[ -e ${esc} ]]
+      then
+        "${coreutils}/bin/rm" -vrf ${esc}
+      fi
+      true  # End with a success code
+    '';
+
+  makeCommands = outputs: p: concatStringsSep "\n"
+                               (map (makeCommand p) outputs);
 };
 rec {
-  def = pkg: paths: lib.overrideDerivation pkg (old: {
+  def = pkg: paths: overrideDerivation pkg (old: {
           builder = writeScript "${old.name}-without-bits" ''
             #!${bash}/bin/bash
             "${old.builder}" "$@"
-            ${lib.concatStringsSep "\n" (map mkCmd paths)}
+            ${concatStringsSep "\n"
+                (map (makeCommands (old.outputs or ["out"]))
+                     paths)}
           '';
         });
 
-  tests = {
-    canRemoveHello = runCommand "can-remove-hello"
-      {
-        buildInputs = [ fail ];
-        p           = def hello [ "bin/hello" ];
-      }
-      ''
-        [[ -e "$p"           ]] || fail "Dir '$p' not found"
-        [[ -e "$p/bin/hello" ]] && fail "Didn't remove '$p/bin/hello'"
-        mkdir "$out"
-      '';
-  };
+  tests =
+    with rec {
+      go = { label, pkg, toRemove }: runCommand "can-remove-${label}"
+        {
+          buildInputs = [ fail ];
+          p           = def pkg toRemove;
+        }
+        ''
+          [[ -e "$p" ]] || fail "Dir '$p' not found"
+          ${concatStringsSep "\n"
+              (map (p: ''[[ -e "$p/${p}" ]] && fail "Didn't remove '$p/${p}'"'')
+                   toRemove)}
+          mkdir "$out"
+        '';
+    };
+    {
+      canRemoveSimple = go {
+        label    = "simple-package";
+        pkg      = hello;
+        toRemove = [ "bin/hello" ];
+      };
+
+      canRemoveMultiOutput = go {
+        label    = "multi-output-derivation";
+        pkg      = mupdf;
+        toRemove = [ "bin/mupdf-gl"  "bin/mupdf-x11-curl" ];
+      };
+    };
 }
