21810616235a613dbb804a992c444ea57ab5f890
Chris Warburton <chriswarbo@gmail.com>
2019-05-31 01:53:13 +0100

    Check attrsToDirs builds referenced paths

--- helpers/attrsToDirs'.nix
+++ helpers/attrsToDirs'.nix
@@ -1,12 +1,17 @@
 # Builds a directory whose entries/content correspond to the names/values of
 # the given attrset. When a value is an attrset, the corresponding entry is
 # a directory, whose contents is generated with attrsToDirs on that value.
-{ addPathToStore, asPath, die, dummyBuild, getType, hello, isPath, lib,
-  nixListToBashArray, nothing, runCmd, sanitiseName }:
+{ addPathToStore, asPath, die, dummyBuild, foldAttrs', getType, hello, isPath,
+  lib, nixListToBashArray, nothing, runCmd, runCommand, sanitiseName,
+  writeScript }:
 
 with builtins;
 with lib;
 with rec {
+  # Traverses into the given nested attrset, producing a list of name/value
+  # pairs, where each value is a path or derivation, and each name is the path
+  # through the attrsets to reach that value. Values which are paths get copied
+  # into the Nix store first.
   toPaths = prefix: val:
     if isPath val
        then [{ name = prefix; value = addPathToStore val; }]
@@ -19,7 +24,7 @@ with rec {
                        else die {
                          error   = "Unsupported type in attrsToDirs'";
                          given   = getType val;
-                         allowed = [ "path" "derivation" "attrset" ];
+                         allowed = [ "path" "derivation" "set" ];
                        };
 };
 rec {
@@ -80,5 +85,45 @@ rec {
     punctuatedStore = def "punctuatedStorePathTest" {
       foo = { bar = "${./.}/attrsToDirs'.nix"; };
     };
+
+    # Check that dependencies of (strings of) paths become dependencies of the
+    # resulting derivation.
+    dependenciesPreserved =
+      with rec {
+        file1 = writeScript "test-file1" "content1";
+        file2 = writeScript "test-file2" "content2";
+        link1 = runCommand  "test-link1" { inherit file1; } ''
+          mkdir "$out"
+          ln -s "$file1" "$out/link1"
+        '';
+        link2 = runCommand  "test-link2" { inherit file2; } ''
+          ln -s "$file2" "$out"
+        '';
+        dir   = def "dirsWithDeps" {
+          entry1 = "${link1}/link1";
+          entry2 = "${link2}";
+        };
+      };
+      runCommand "dependenciesPreservedTest" { inherit dir; } ''
+        function fail {
+          echo "$*" 1>&2
+          exit 1
+        }
+
+        [[ -d "$dir"        ]] || fail "No such directory '$dir'"
+        [[ -e "$dir"/entry1 ]] || fail "No entry1 in '$dir'"
+        [[ -e "$dir"/entry2 ]] || fail "No entry2 in '$dir'"
+
+        file1=$(readlink -f "$dir"/entry1)
+        file2=$(readlink -f "$dir"/entry2)
+
+        [[ -e "$file1" ]] || fail "Destination '$file1' not found"
+        [[ -e "$file2" ]] || fail "Destination '$file2' not found"
+
+        grep 'content1' < "$file1" > /dev/null || fail "No content in '$file1'"
+        grep 'content2' < "$file2" > /dev/null || fail "No content in '$file2'"
+
+        mkdir "$out"
+      '';
   };
 }
