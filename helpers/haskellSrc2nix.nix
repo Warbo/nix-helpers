cdbdd42aa3a75d8688ed5b014359ea434e8e0179
Chris Warburton <chriswarbo@gmail.com>
2019-06-09 11:35:32 +0100

    Don't pin haskellSrc2nix

--- helpers/haskellSrc2nix.nix
+++ helpers/haskellSrc2nix.nix
@@ -1,9 +1,6 @@
-{ nixpkgs1803 }:
+{ haskellPackages }:
 
-with builtins;
 {
-  def   = trace "FIXME: cabal2nix broken on 18.09 due to broken yaml package"
-                nixpkgs1803.haskellPackages.haskellSrc2nix;
-
+  def   = haskellPackages.haskellSrc2nix;
   tests = {};
 }
