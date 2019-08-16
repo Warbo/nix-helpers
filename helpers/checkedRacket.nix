5b478224b2c1344de8a4c1d3f0d3e574eca5f2d9
Chris Warburton <chriswarbo@gmail.com>
2019-08-16 10:57:49 +0100

    Allow checkedRacket to be overridden
    We don't want to hard-code the fallback 'racket' package in 'checkedRacket',
    since that will cause an infinite loop if we try to override the system-wide
    'racket' package to be 'checkedRacket'.
    
    By using 'makeOverridable', we can break that loop if the situation arises.

--- helpers/checkedRacket.nix
+++ helpers/checkedRacket.nix
@@ -3,13 +3,19 @@
 with builtins;
 with lib;
 with checkRacket;
+with {
+  # The racket argument can be overridden to break infinite loops, e.g. if we
+  # want to replace 'racket' with 'checkedRacket' system-wide (in which case
+  # 'then racket' will loop)
+  go = { racket }: if racketWorks
+                      then racket
+                      else trace (concatStringsSep " " [
+                                   "WARNING: Taking racket from nixpkgs 16.09,"
+                                   "since newer versions are marked as broken on"
+                                   "i686 nixpkgs."])
+                                 nixpkgs1609.racket;
+};
 {
-  def   = if racketWorks
-             then racket
-             else trace (concatStringsSep " " [
-                          "WARNING: Taking racket from nixpkgs 16.09,"
-                          "since newer versions are marked as broken on"
-                          "i686 nixpkgs."])
-                        nixpkgs1609.racket;
+  def   = makeOverridable go { inherit racket; };
   tests = {};
 }
