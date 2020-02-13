ab0c3db1531b51bf43fae554ba22f2c87770b2fa
Chris Warburton <chris@zipabout.com>
2020-02-13 06:34:40 +0000

    Bump haskell-nix to avoid missing commit error
    On macOS I'm hitting an error when one of the haskell-nix imports tries to fetch
    iohk's fork of Nix, saying that the specified commit doesn't exist. Using the
    latest haskell.nix fixed it, so I've rebased my fork on that. This commit points
    nix-helpers to this rebased fork.

--- helpers/haskell-nix.nix
+++ helpers/haskell-nix.nix
@@ -4,8 +4,8 @@
 with {
   src = builtins.fetchTarball {
     name   = "haskell-nix";
-    url    = https://github.com/Warbo/haskell.nix/archive/499a761.tar.gz;
-    sha256 = "1pnkywswfa71hgc2c3g2cijfk9nysbpyh6jjh455h810n4yhs522";
+    url    = https://github.com/Warbo/haskell.nix/archive/7f7901d.tar.gz;
+    sha256 = "1271g9yf77x430jdiy85v43qa8n9n6w0jlprsrv9l6kwv0qs7iv4";
   };
 };
 {
