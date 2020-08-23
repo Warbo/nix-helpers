b3003072c02329cba11ca7118ad22e2437184dad
Chris Warburton <chriswarbo@gmail.com>
2020-08-23 10:34:16 +0100

    Manage haskell-nix with niv

--- helpers/haskell-nix.nix
+++ helpers/haskell-nix.nix
@@ -1,16 +1,9 @@
 # IOHK's haskell.nix infrastructure
-{ repo1909 }:
+{ nix-helpers-sources, repo1909 }:
 
-with {
-  src = builtins.fetchTarball {
-    name   = "haskell-nix";
-    url    = https://github.com/Warbo/haskell.nix/archive/499a761.tar.gz;
-    sha256 = "1pnkywswfa71hgc2c3g2cijfk9nysbpyh6jjh455h810n4yhs522";
-  };
-};
 {
-  def   = { repo ? repo1909 }: import repo {
-            overlays = import "${src}/overlays";
-          };
+  def = { repo ? repo1909 }: import repo {
+    overlays = import "${nix-helpers-sources.haskell-nix.outPath}/overlays";
+  };
   tests = {};
 }
