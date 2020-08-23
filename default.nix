924c5ea3f2615cb353577a21e56501211c100d16
Chris Warburton <chriswarbo@gmail.com>
2020-08-23 09:58:36 +0100

    Use whichever nixpkgs is latest in default.nix
    Rather than using <nixpkgs>, which is impure

--- default.nix
+++ default.nix
@@ -1,7 +1,25 @@
-with {
-  pkgs = import <nixpkgs> {
+# Returns nix-helpers from the latest entry in ./nixpkgs.nix
+with rec {
+  inherit (builtins) attrNames compareVersions getAttr;
+  inherit (bootstrap) lib;
+  inherit (lib) fold hasPrefix;
+  inherit (import ./nixpkgs.nix {} { inherit lib; }) defs;
+
+  # Some arbitrary nixpkgs repo, so we can use its 'lib'
+  bootstrap = import (import ./nix/sources.nix).repo1909.outPath {
     config   = {};
-    overlays = [ (import ./overlay.nix) ];
+    overlays = [];
   };
+
+  # Find whichever repoXXXX entry has the newest version
+  latest = fold (name: found: if found == null ||
+                                 compareVersions name found == 1
+                                 then name
+                                 else found)
+                null
+                (attrNames defs);
 };
-pkgs.nix-helpers
+(import (getAttr latest defs) {
+  config   = {};
+  overlays = [ (import ./overlay.nix) ];
+}).nix-helpers
