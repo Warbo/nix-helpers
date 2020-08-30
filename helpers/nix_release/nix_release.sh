c169db2060f1d099cf78fe0ad06c24948075e7de
Chris Warburton <chriswarbo@gmail.com>
2020-08-30 15:02:22 +0100

    Boolean was wrong way round

--- helpers/nix_release/nix_release.sh
+++ helpers/nix_release/nix_release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -z "$ADD_ROOT" ]]
+if [[ -n "$ADD_ROOT" ]]
 then
     [[ -e "$ADD_ROOT" ]] || fail "ADD_ROOT dir '$ADD_ROOT' doesn't exist"
     echo "GC roots will be made in '$ADD_ROOT'" 1>&2
@@ -25,12 +25,12 @@ do
       DRV=$(echo "$PAIR" | cut -f2)
 
     echo "Building $ATTR" 1>&2
-    if [[ -z "$ADD_ROOT" ]]
+    if [[ -n "$ADD_ROOT" ]]
     then
-        build                                         "$@" "$DRV" ||
+        build --indirect --add-root "$ADD_ROOT/$ATTR" "$@" "$DRV" ||
             FAILS=$(( FAILS + 1 ))
     else
-        build --indirect --add-root "$ADD_ROOT/$ATTR" "$@" "$DRV" ||
+        build                                         "$@" "$DRV" ||
             FAILS=$(( FAILS + 1 ))
     fi
 done < <(echo "$DRVPATHS")
