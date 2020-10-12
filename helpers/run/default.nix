b22cb2c5641512d2579cfc562b5b9572ceea545a
Chris Warburton <chris@zipabout.com>
2020-10-12 19:24:58 +0100

    Allow overriding derivation arguments of 'run'

--- helpers/run/default.nix
+++ helpers/run/default.nix
@@ -1,5 +1,14 @@
 { bash, ghc, nixpkgs1609, python, runCommand, wrap }:
 
-args: runCommand args.name {} (wrap (args // {
-  name = "${args.name}-runner";
-}))
+args:
+  with {
+    # Allow derivation arguments to be given/overridden
+    drvArgs = args.drvExtras or {};
+
+    # Arguments for the command we're running. By default we remove fooExtras
+    # and invent a name; this can be overridden by setting 'cmdExtras'.
+    cmdArgs = builtins.removeAttrs args [ "cmdExtras" "drvExtras" ] // {
+      name = "${args.name}-runner";
+    } // (args.cmdExtras or {});
+  };
+  runCommand args.name drvArgs (wrap cmdArgs)
