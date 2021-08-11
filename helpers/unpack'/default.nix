000807cd56daad829f9ff216bb7e7f5eebb5aefa
Chris Warburton <chris@zipabout.com>
2021-08-11 10:19:06 +0100

    Fixes to unpack'

--- helpers/unpack'/default.nix
+++ helpers/unpack'/default.nix
@@ -2,16 +2,22 @@
 { runCommand }:
 
 name: src: runCommand name { inherit src; } ''
+  isTar() {
+    echo "$1" | grep -i '\.tar' 1>/dev/null && return 0
+    echo "$1" | grep -i '\.tgz' 1>/dev/null && return 0
+    return 1
+  }
+
   if [[ -d "$src" ]]
   then
     cp -r "$src" "$out"
-  else if echo "$src" | grep '\.tar' 1>/dev/null
-       then
-         # Extract top-level directory (whatever it's called) to $out
-         mkdir "$out"
-         tar xf "$src" -C "$out" --strip-components 1
-       else
-         cp -r "$src" "$out"
-       fi
+  else if isTar "$src"
+    then
+      # Extract top-level directory (whatever it's called) to $out
+      mkdir "$out"
+      tar xf "$src" -C "$out" --strip-components 1
+    else
+      cp -r "$src" "$out"
+    fi
   fi
 ''
