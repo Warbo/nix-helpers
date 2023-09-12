ae290fb7be415f6b9e3a9b1e87413b00655f072b
Chris Warburton <chriswarbo@gmail.com>
2023-09-12 01:17:24 +0100

    Bootstrap using nixpkgs-lib; make default.nix a function of nixpkgs

--- default.nix
+++ default.nix
@@ -1,14 +1,6 @@
-# Returns nix-helpers from the latest entry in ./nixpkgs.nix
-with rec {
-  # Some arbitrary nixpkgs repo, so we can use its 'lib'
-  bootstrap = import (import ./nix/sources.nix).repo1909 {
-    config   = {};
-    overlays = [];
-  };
-
-  nixpkgs = import ./nixpkgs.nix { inherit (bootstrap) lib; };
-};
-(import nixpkgs.repoLatest {
-  config   = {};
+{ nixpkgs ?
+  (import ./nixpkgs.nix { lib = import helpers/nixpkgs-lib { }; }).repoLatest }:
+(import nixpkgs {
+  config = { };
   overlays = [ (import ./overlay.nix) ];
 }).nix-helpers
