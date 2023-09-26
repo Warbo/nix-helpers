9bed246bfc3694e42e50f77b487fcb978a7eaaeb
Chris Warburton <chriswarbo@gmail.com>
2023-09-26 12:05:05 +0100

    Bump nixpkgs-lib for fileset updates

--- helpers/nixpkgs-lib/default.nix
+++ helpers/nixpkgs-lib/default.nix
@@ -1,9 +1,11 @@
 # Standalone copy of Nixpkgs's "lib" attrset: smaller than depending on Nixpkgs!
-{ rev ? "c30b6a84c0b84ec7aecbe74466033facc9ed103f"
-, sha256 ? "sha256:194p8gfn5pvwq1naz47z6z7ncpr4bkxicjik756sw8sfw96f99yv", src ?
-  fetchTarball {
-    inherit sha256;
-    name = "nixpkgs-lib";
-    url = "https://github.com/nix-community/nixpkgs.lib/archive/${rev}.tar.gz";
-  } }:
-import "${src}/lib"
+{ nixpkgs-lib-rev ? "01fc4cd75e577ac00e7c50b7e5f16cd9b6d633e8"
+, nixpkgs-lib-sha256 ?
+  "sha256:10l1ndysycnfwfxrhchvvjdf1lwn7kj8f89cxwzvnf5m5grfdgm4"
+, nixpkgs-lib-src ? fetchTarball {
+  name = "nixpkgs-lib";
+  url = "https://github.com/nix-community/nixpkgs.lib/archive/"
+    + nixpkgs-lib-rev + ".tar.gz";
+  sha256 = nixpkgs-lib-sha256;
+} }:
+import "${nixpkgs-lib-src}/lib"
