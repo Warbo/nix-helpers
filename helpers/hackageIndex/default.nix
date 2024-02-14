f9a6dfdc2ede25e77bdbd8d84ea4f2b7ce999e36
Chris Warburton <chriswarbo@gmail.com>
2024-02-14 12:11:34 +0000

    Add hackageIndex

---
+++ helpers/hackageIndex/default.nix
@@ -0,0 +1,16 @@
+{ all-cabal-hashes, haskellPackages, lib, runCommand, writeScript }:
+
+with {
+  rev = lib.removePrefix "all-cabal-hashes-" all-cabal-hashes.name;
+
+  mkHackageIndex = writeScript "mkHackageIndex" ''
+    #!${
+      haskellPackages.ghcWithPackages
+      (pkgs: [ pkgs.aeson pkgs.MissingH pkgs.tar ])
+    }/bin/runhaskell
+    ${builtins.readFile ./mkHackageIndex.hs}
+  '';
+};
+runCommand "01-index-${rev}" { } ''
+  < ${all-cabal-hashes} gunzip | ${mkHackageIndex} | gzip > "$out"
+''
