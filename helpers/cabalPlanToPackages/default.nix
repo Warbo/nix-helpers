d21c167b36d00e47f4626ac34bd705aba088187b
Chris Warburton <chriswarbo@gmail.com>
2024-02-14 12:12:06 +0000

    Add cabalPlanToPackages

---
+++ helpers/cabalPlanToPackages/default.nix
@@ -0,0 +1,25 @@
+{ haskell, haskellPackages }:
+with {
+  inherit (builtins) filter listToAttrs map;
+  oldHaskellPackages = haskellPackages;
+};
+{ cabalPlan, haskellPackages ? oldHaskellPackages }:
+with rec {
+  namesToVersions = map (pkg: {
+    name = pkg.pkg-name;
+    value = pkg.pkg-version;
+  });
+
+  chosenVersions = haskell.lib.packageSourceOverrides (listToAttrs
+    (namesToVersions (filter (pkg: pkg.type != "pre-existing")
+      (cabalPlan.install-plan or cabalPlan.json.install-plan))));
+
+  fixes = self: super: {
+    # TODO: Disable all tests, to prevent circular dependencies
+    mkDerivation = args: super.mkDerivation (args // { doCheck = false; });
+
+    # The splitmix package lists 'testu01' as a required "system dependency"
+    testu01 = null;
+  };
+};
+(haskellPackages.extend fixes).extend chosenVersions
