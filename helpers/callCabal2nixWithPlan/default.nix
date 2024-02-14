08c4cb61d0768fe1d8e363a6f8617bfb3cf068fc
Chris Warburton <chriswarbo@gmail.com>
2024-02-14 18:06:11 +0000

    callCabal2nixWithPlan: Use original mkDerivation to enable tests

--- helpers/callCabal2nixWithPlan/default.nix
+++ helpers/callCabal2nixWithPlan/default.nix
@@ -10,7 +10,14 @@ with {
   mkCabalPlan { inherit cabalFile doBench doCheck hackageIndex name; } }:
 with rec {
   planPackages = cabalPlanToPackages { inherit cabalPlan haskellPackages; };
-  result = planPackages.callCabal2nix name src { };
+  result = planPackages.callCabal2nix name src
+    # cabalPlanToPackages overrides mkDerivation to disable tests, to prevent
+    # circular dependencies. The original mkDerivation function is kept in an
+    # attribute: if it's present, use it (hence allowing this package's tests)
+    (if planPackages.mkDerivation ? original then {
+      mkDerivation = planPackages.mkDerivation.original;
+    } else
+      { });
 };
 result // {
   inherit cabalPlan hackageIndex;
