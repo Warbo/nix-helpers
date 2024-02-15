5f0639b37a5f45a7d6f22c5811983836faca8545
Chris Warburton <chriswarbo@gmail.com>
2024-02-15 20:34:48 +0000

    callCabal2nixWithPlan: Add test build of json-to-msgpack

---
+++ helpers/callCabal2nixWithPlan/tests.nix
@@ -0,0 +1,14 @@
+{ callCabal2nixWithPlan }: {
+  can-build-json-to-msgpack = with {
+    # Try to build some Haskell project; this one's nice and small
+    src = fetchGit {
+      url = "http://chriswarbo.net/git/json-to-msgpack.git";
+      ref = "master";
+      rev = "07bf1f3ddc7c46fae7095ec8e496defc357882ce";
+    };
+  };
+    callCabal2nixWithPlan {
+      inherit src;
+      name = "json-to-msgpack";
+    };
+}
