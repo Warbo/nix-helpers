c9756ac9e944a24291d9eba8d57f67ccf095a35e
Chris Warburton <chriswarbo@gmail.com>
2024-02-20 19:55:05 +0000

    inNixedDir: Provide file

--- helpers/inNixedDir/default.nix
+++ helpers/inNixedDir/default.nix
@@ -1,42 +1,15 @@
 { bash, mkBin, withNix }:
 
 with builtins;
-with { nixy = withNix { }; };
+with {
+  nixy = withNix { };
+  file = ./inNixedDir.sh;
+};
 mkBin {
+  inherit file;
   name = "inNixedDir";
   paths = nixy.buildInputs;
   vars = removeAttrs nixy [ "buildInputs" ];
-  script = ''
-    #!${bash}/bin/bash
-    set -e
-
-    # Runs the given command in a temporary directory, adds that directory
-    # to the Nix store then deletes the temp directory. If a second argument
-    # is given, it's used as the directory name (which Nix prefixes with a
-    # content hash).
-
-    NAME="nixed-dir"
-    [[ -z "$2" ]] || NAME="$2"
-
-    if [[ "$SKIP_NIX" -eq 1 ]]
-    then
-      INNER="$NAME"
-    else
-      SCRATCH=$(mktemp -d)
-      trap "rm -rf $SCRATCH" EXIT
-      INNER="$SCRATCH/$NAME"
-    fi
-
-    mkdir -p "$INNER"
-    pushd "$INNER" > /dev/null
-      out="$PWD" "$1"
-    popd           > /dev/null
-
-    if [[ "$SKIP_NIX" -eq 1 ]]
-    then
-      readlink -f "$INNER"
-    else
-      nix-store --add "$INNER"
-    fi
-  '';
+} // {
+  file = "${file}";
 }
