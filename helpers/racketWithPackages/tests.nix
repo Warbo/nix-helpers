12c29bbab499054837b4ad2701d3e56ca581bbdb
Chris Warburton <Chris.Warburton@tracsis.com>
2024-06-08 08:04:00 +0000

    tests: remove raco packages that don't work with latest racket

--- helpers/racketWithPackages/tests.nix
+++ helpers/racketWithPackages/tests.nix
@@ -7,26 +7,12 @@
 
 with {
   result = racketWithPackages [
-    # Dependency of grommet
-    (fetchgit {
-      url = "https://gitlab.com/RayRacine/grip.git";
-      rev = "ec498f6";
-      sha256 = "06ax30r70sz2hq0dzyassczcdkpmcd4p62zx0jwgc2zp3v0wl89l";
-    })
-
-    # Hashing
-    (fetchgit {
-      url = "https://gitlab.com/RayRacine/grommet.git";
-      rev = "50f1b6a";
-      sha256 = "1rb7i8jx7gg2rm5flnql0hja4ph11p7i38ryxd04yqw50l0xj59v";
-    })
-
     # Shell commands
     (fetchFromGitHub {
       owner = "willghatch";
       repo = "racket-shell-pipeline";
-      rev = "7ed9a75";
-      sha256 = "06z5bhmvpdhy4bakh30fzha4s0xp2arjq8h9cyi65b1y18cd148x";
+      rev = "5f4232b58552c0affee15612f93629c5d66db7ea";
+      sha256 = "1kn3aflv7z44m65qj1jjjvvkh1d3sbwywscqd6y9gqpjkvfxwib3";
     })
   ];
 };
