39549698e65a681d422104161a579cfaec8e33ab
Chris Warburton <chriswarbo@gmail.com>
2025-07-10 09:10:13 +0100

    withArgsOf: Refactor

--- helpers/withArgsOf/default.nix
+++ helpers/withArgsOf/default.nix
@@ -1,9 +1,13 @@
-{ withArgs }:
-
-with builtins;
+{
+  lib ? import ../nixpkgs-lib {},
+  withArgs ? import ../withArgs { inherit lib; }
+}:
 
 # Return an eta-expanded wrapper around "g", which accepts the same named
 # arguments as "f".
 f: g:
-with { fArgs = functionArgs f; };
+with rec {
+  inherit (builtins) attrNames filter functionArgs;
+  fArgs = functionArgs f;
+};
 withArgs (filter (n: !fArgs."${n}") (attrNames fArgs)) g
