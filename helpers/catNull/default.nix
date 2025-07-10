79064a5350f05b7ce95c0b4983b0cb36745e2e9e
Chris Warburton <chriswarbo@gmail.com>
2025-07-10 16:11:59 +0100

    catNull: Support attrsets

--- helpers/catNull/default.nix
+++ helpers/catNull/default.nix
@@ -1,3 +1,10 @@
-# Removes null values from a list
-{}:
-builtins.concatMap (x: if x == null then [] else [x])
+# Removes null values from a list or attrset
+{ lib }:
+with {
+  inherit (builtins) concatMap isAttrs isList;
+};
+xs: if isList xs
+    then concatMap (x: if x == null then [] else [x]) xs
+    else if isAttrs xs
+    then lib.filterAttrs (_: value: value != null) xs
+    else abort "catNull needs a list or attrset"
