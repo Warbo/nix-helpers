1e8309aee1865576c19afb7b8d8140b98156f657
Chris Warburton <chriswarbo@gmail.com>
2025-11-26 16:07:42 +0000

    nixpkgs-lib: structure so update-nix-fetchgit can update it

--- helpers/nixpkgs-lib/default.nix
+++ helpers/nixpkgs-lib/default.nix
@@ -1,14 +1,25 @@
 # Standalone copy of Nixpkgs's "lib" attrset: smaller than depending on Nixpkgs!
+with rec {
+  fetchFromGitHub = args: args; # Lets us use update-nix-fetchgit
+
+  def = fetchFromGitHub {
+    owner = "nix-community";
+    repo = "nixpkgs.lib";
+    rev = "01fc4cd75e577ac00e7c50b7e5f16cd9b6d633e8";
+    sha256 = "sha256:10l1ndysycnfwfxrhchvvjdf1lwn7kj8f89cxwzvnf5m5grfdgm4";
+  };
+
+  get =
+    rev: sha256:
+    fetchTarball {
+      inherit sha256;
+      name = "nixpkgs-lib";
+      url = "https://github.com/${def.owner}/${def.repo}/archive/${rev}.tar.gz";
+    };
+};
 {
-  nixpkgs-lib-rev ? "01fc4cd75e577ac00e7c50b7e5f16cd9b6d633e8",
-  nixpkgs-lib-sha256 ? "sha256:10l1ndysycnfwfxrhchvvjdf1lwn7kj8f89cxwzvnf5m5grfdgm4",
-  nixpkgs-lib-src ? fetchTarball {
-    name = "nixpkgs-lib";
-    url =
-      "https://github.com/nix-community/nixpkgs.lib/archive/"
-      + nixpkgs-lib-rev
-      + ".tar.gz";
-    sha256 = nixpkgs-lib-sha256;
-  },
+  nixpkgs-lib-rev ? def.rev,
+  nixpkgs-lib-sha256 ? def.sha256,
+  nixpkgs-lib-src ? get nixpkgs-lib-rev nixpkgs-lib-sha256,
 }:
 import "${nixpkgs-lib-src}/lib"
