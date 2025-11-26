dd4d051b8c8baed8523864922c6f67577c5fcab1
Chris Warburton <chriswarbo@gmail.com>
2025-11-26 11:49:11 +0000

    git-hooks: Enable more out-of-the-box pre-commit hooks

--- helpers/git-hooks/defaultHooks.nix
+++ helpers/git-hooks/defaultHooks.nix
@@ -1,18 +1,41 @@
 {
   black.enable = true;
+  cabal-fmt.enable = true;
+  cargo-check.enable = true;
+  check-added-large-files.enable = true;
+  check-executables-have-shebangs.enable = true;
+  check-json.enable = true;
+  check-merge-conflicts.enable = true;
+  check-shebang-scripts-are-executable.enable = true;
+  check-toml.enable = true;
+  check-xml.enable = true;
+  check-yaml.enable = true;
   deadnix.enable = true;
+  editorconfig-checker.enable = true;
+  end-of-file-fixer.enable = true;
+  eslint.enable = true;
   flake8.enable = true;
   fourmolu.enable = true;
+  gofmt.enable = true;
   hlint.enable = true;
+  html-tidy.enable = true;
   markdownlint.enable = true;
+  mixed-line-endings.enable = true;
   mypy.enable = true;
-  nixfmt-rfc-style.enable = true;
-  nixfmt-rfc-style.settings.width = 80;
-  pretty-format-json.enable = true;
+  nixfmt-rfc-style = {
+    enable = true;
+    settings.width = 80;
+  };
+  pretty-format-json = {
+    enable = true;
+    settings.autofix = true;
+  };
   pylint.enable = true;
   rustfmt.enable = true;
   shellcheck.enable = true;
   shfmt.enable = true;
+  statix.enable = true;
+  trim-trailing-whitespace.enable = true;
   yamlfmt.enable = true;
   yamllint.enable = true;
 }
