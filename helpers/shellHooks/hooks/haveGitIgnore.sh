a5a95b04d67c7fcc61d0d7d2af44d6d83506fc93
Chris Warburton <chriswarbo@gmail.com>
2025-11-26 02:16:58 +0000

    shellHooks: gitignore .editorconfig

--- helpers/shellHooks/hooks/haveGitIgnore.sh
+++ helpers/shellHooks/hooks/haveGitIgnore.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 (
-	GIT_DIR=$(git rev-parse --show-toplevel) || exit 0
-	GI="$GIT_DIR/.gitignore"
-	[[ -e $GI ]] || touch "$GI"
+  GIT_DIR=$(git rev-parse --show-toplevel) || exit 0
+  GI="$GIT_DIR/.gitignore"
+  [[ -e $GI ]] || {
+    echo "Creating '$GI'" 1>&2
+    touch "$GI"
+  }
 
-	for LINE in '/.pre-commit-config.yaml'; do
-		grep -q -Fx "$LINE" <"$GI" ||
-			printf '%s\n' "$LINE" >>"$GI"
-	done
+  PRESETS=()
+  PRESETS+=('/.pre-commit-config.yaml')
+  PRESETS+=('/.editorconfig')
+  for LINE in "${PRESETS[@]}"; do
+    grep -q -Fx "$LINE" <"$GI" ||
+      printf '%s\n' "$LINE" >>"$GI"
+  done
 )
