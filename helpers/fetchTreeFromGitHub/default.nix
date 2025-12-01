d9d5bf38e3369bdd60557fb6a4d136afb3da9ca0
Chris Warburton <chriswarbo@gmail.com>
2025-12-01 22:13:00 +0000

    fetchTreeFromGitHub: Remove Nixpkgs dependency, use builtin builders
    Removing the dependency on Nixpkgs is useful, since we want to use this to FETCH
    Nixpkgs from github in the first place! Builtin builders let us do this, though
    it's a bit convoluted, since we can unpack the tarball using 'unpack-channel',
    but that puts the result in a subdirectory; so we have to calculate an extra
    SHA1 for that wrapper.
    
    We can do that now that we've got gitTreeSingleton

--- helpers/fetchTreeFromGitHub/default.nix
+++ helpers/fetchTreeFromGitHub/default.nix
@@ -1,21 +1,45 @@
 # Like fetchFromGitHub but uses the given 'rev' as both the ID and outputHash.
 # This only works when 'rev' is a Git tree, not a Git commit; to find the tree
 # associated with a commit X, use 'git rev-parse X^{tree}'.
-{ nixpkgs }:
+{
+  gitTreeSingleton ? import ../gitTreeSingleton { },
+}:
 {
   owner,
   repo,
   tree,
 }:
-(nixpkgs.fetchFromGitHub {
-  inherit owner repo;
-  rev = tree;
-  hash = builtins.convertHash {
-    hash = tree;
-    hashAlgo = "sha1";
-    toHashFormat = "sri";
-  };
-}).overrideAttrs
-  (_: {
+with {
+  inherit (builtins)
+    convertHash
+    fetchurl
+    hashFile
+    path
+    ;
+  channelName = "${owner}-${repo}";
+};
+"${
+  derivation {
+    inherit channelName;
+    name = "${owner}-${repo}-${tree}-unpacked";
+    builder = "builtin:unpack-channel";
+    system = "builtin";
+    outputHashAlgo = "sha1";
     outputHashMode = "git";
-  })
+    outputHash = convertHash {
+      # The output of unpack-channel won't have tree as its SHA1 since it will
+      # be wrapped in a directory (whose name matches channelName). However, we
+      # can calculate the SHA1 of that wrapper, using tree and channelName!
+      hash = hashFile "sha1" (gitTreeSingleton {
+        name = channelName;
+        sha1 = tree;
+      });
+      hashAlgo = "sha1";
+      toHashFormat = "sri";
+    };
+    src = fetchurl {
+      name = "${owner}-${repo}-${tree}.tar.gz";
+      url = "https://github.com/${owner}/${repo}/archive/${tree}.tar.gz";
+    };
+  }
+}/${channelName}"
