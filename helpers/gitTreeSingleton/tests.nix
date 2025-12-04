0224229f0758a55742ac1f2613357d5d4560a4d6
Chris Warburton <chris.warburton@tracsis.com>
2025-12-04 16:13:16 +0000

    gitTreeSingleton: Fix test

--- helpers/gitTreeSingleton/tests.nix
+++ helpers/gitTreeSingleton/tests.nix
@@ -18,30 +18,27 @@ with {
     };
 };
 {
-  valid-git-object = test {
-    name = "valid-git-object";
-    script = ''
-      set -u
-      git init -q
-      tree=${
-        gitTreeSingleton {
-          name = "test";
-          sha1 = "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391";
-        }
+  valid-git-object = test "valid-git-object" ''
+    set -u
+    git init -q
+    tree=${
+      gitTreeSingleton {
+        name = "test";
+        sha1 = "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391";
       }
-      sha=$(sha1sum "$tree" | cut -d' ' -f1)
-      obj_dir=".git/objects/$(echo "$sha" | cut -c1-2)"
-      obj_file="$obj_dir/$(echo "$sha" | cut -c3- | tr -d '\n')"
-      mkdir -p "$obj_dir"
-      zlib-flate -compress < "$tree" > "$obj_file"
-      type=$(git cat-file -t "$sha")
-      [[ "$type" == "tree" ]] || {
-        echo "Expected 'tree', got '$type'"
-        exit 1
-      } 1>&2
-      mkdir "$out"
-    '';
-  };
+    }
+    sha=$(sha1sum "$tree" | cut -d' ' -f1)
+    obj_dir=".git/objects/$(echo "$sha" | cut -c1-2)"
+    obj_file="$obj_dir/$(echo "$sha" | cut -c3- | tr -d '\n')"
+    mkdir -p "$obj_dir"
+    zlib-flate -compress < "$tree" > "$obj_file"
+    type=$(git cat-file -t "$sha")
+    [[ "$type" == "tree" ]] || {
+      echo "Expected 'tree', got '$type'"
+      exit 1
+    } 1>&2
+    mkdir "$out"
+  '';
 
   bash-matches-dash =
     runCommand "gitTreeSingleton-test-bash-matches-dash"
